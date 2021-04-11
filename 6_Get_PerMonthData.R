1e75402cee91ec94213b06385a363378d93ba30d
JLLucas <39839798+jingliu0928@users.noreply.github.com>
2021-04-11 02:27:26 -0400

    Create 6_Get_PerMonthData.R

---
+++ 6_Get_PerMonthData.R
@@ -0,0 +1,77 @@
+proj_dir <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/"
+source(paste0(proj_dir,"ReCAPSE_Code/Ultilities.R"))
+data_dir <- paste0(proj_dir,"/ReCAPSE_Intermediate_Data/0318_21/For_Both_Data/")
+outdir <- paste0(proj_dir,"/ReCAPSE_Intermediate_Data/0318_21/For_Both_Data/")
+
+
+################################################################################ 
+#### Load combined per day data
+################################################################################ 
+Comb_perday_df <- read.csv(paste0(data_dir, "Comb_PerDay_data.csv"),stringsAsFactors = F)
+Comb_perday_df <- Comb_perday_df[,-1]
+
+################################################################################ 
+### Load Valid Month
+################################################################################ 
+Valid_Month_df <- read.csv(paste0(outdir,"All_Final_Valid_month.csv"),stringsAsFactors = F)
+length(unique(Valid_Month_df[,"ID"])) #28675
+
+################################################################################ 
+#Filter by valid month, get monthly code counts table
+################################################################################ 
+analysis_ID <- unique(intersect(Comb_perday_df[,"study_id"],Valid_Month_df[,"ID"])) #26933
+length(analysis_ID)
+
+n_valid_month <- NA
+valid_perday_df_list <- list()
+for (i in 1:length(analysis_ID)){
+  curr_id <- analysis_ID[i]
+  curr_enroll_months <- unique(Valid_Month_df[which(Valid_Month_df[,"ID"] == curr_id),"Valid_Month"])
+  n_valid_month[i] <- length(curr_enroll_months)
+  
+  min_month <- min(ymd(curr_enroll_months))
+  max_month <- max(ymd(curr_enroll_months))
+  
+  #Curr_per_day  data
+  curr_perday_df <- Comb_perday_df[which(Comb_perday_df[,"study_id"] == curr_id),]
+  
+  index_tokeep <- which(ymd(curr_perday_df[,"claims_date"]) >= min_month & ymd(curr_perday_df[,"claims_date"]) <= max_month )
+  if (length(index_tokeep) > 0 ){
+    valid_perday_df_list[[i]] <- curr_perday_df[index_tokeep,]
+  }else{
+    valid_perday_df_list[[i]] <- NULL
+  }
+  
+}
+
+
+################################################################################ 
+#'TODO Need space to hold this
+#Per month codes count
+n_valid_month <- NA
+for (i in 1:length(analysis_ID)){
+  curr_id <- analysis_ID[i]
+  curr_enroll_months <- unique(Valid_Month_df[which(Valid_Month_df[,"ID"] == curr_id),"Valid_Month"])
+  n_valid_month[i] <- length(curr_enroll_months)
+  
+  #Curr_per_day  data
+  curr_perday_df <- Comb_perday_df[which(Comb_perday_df[,"study_id"] == curr_id),]
+  
+  #per month table for diag, pro and drug
+  res <- create_perMon_count_table(curr_id,n_valid_month,curr_enroll_months,curr_perday_df)
+  curr_pt_month_table_diag <- res[[1]]
+  curr_pt_month_table_proc <- res[[2]]
+  curr_pt_month_table_drug <- res[[3]]
+  
+  outdir1 <- paste0(outdir,"PerMonth_CountTable/Diag/")
+  write.csv(curr_pt_month_table_diag,paste0(outdir1,curr_id,".csv"))
+  outdir2 <- paste0(outdir,"PerMonth_CountTable/Proc/")
+  write.csv(curr_pt_month_table_proc,paste0(outdir2,curr_id,".csv"))
+  outdir3 <- paste0(outdir,"PerMonth_CountTable/Drug/")
+  write.csv(curr_pt_month_table_drug,paste0(outdir3,curr_id,".csv"))
+  
+}
+
+################################################################################ 
+#'TODO: Add patient outcome, pre-post recurrence, first primary dates
+################################################################################ 
