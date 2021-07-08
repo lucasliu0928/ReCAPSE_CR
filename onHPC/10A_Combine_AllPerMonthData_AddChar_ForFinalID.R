c33650ecb4de0c3c7dc8a8bcc3f1d1c0d2e37262
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-07-08 05:10:42 -0400

    Update 10A_Combine_AllPerMonthData_AddChar_ForFinalID.R

--- onHPC/10A_Combine_AllPerMonthData_AddChar_ForFinalID.R
+++ onHPC/10A_Combine_AllPerMonthData_AddChar_ForFinalID.R
@@ -8,29 +8,31 @@ library(doParallel)
 #onHPC
 perday_dir <- "/recapse/intermediate_data/3_perDay_PerPatientData/"
 perMonth_dir <- "/recapse/intermediate_data/6_perMonthData_inValidMonth_perPatientData/"
-outdir <- "/recapse/intermediate_data/"
+data_dir <- "/recapse/intermediate_data/"
+outdir <- "/recapse/intermediate_data/10_perMonthData_withChar/"
 
 # #local
 # perday_dir <- "/Users/lucasliu/Desktop/intermediate_data/3_perDay_PerPatientData/"
 # perMonth_dir <- "/Users/lucasliu/Desktop/intermediate_data/6_perMonthData_inValidMonth_perPatientData/"
-# outdir <- "/Users/lucasliu/Desktop/intermediate_data/"
-
+# data_dir <- "/Users/lucasliu/Desktop/intermediate_data/"
+# outdir <- "/Users/lucasliu/Desktop/intermediate_data/10_perMonthData_withChar/"
+# 
 
 ################################################################################ 
 #1. Load patient level charateristics
 ################################################################################ 
-Patient_Char_df <- read.xlsx(paste0(outdir,"8_PatientLevel_charecteristics.xlsx"),sheet = 1)
+Patient_Char_df <- read.xlsx(paste0(data_dir,"8_PatientLevel_charecteristics.xlsx"),sheet = 1)
 
 ################################################################################ 
 #2. Load Anlaysis ID
 ################################################################################ 
-FinalID_df <- read.xlsx(paste0(outdir,"9_Final_Analysis_ID.xlsx"),sheet = 1)
+FinalID_df <- read.xlsx(paste0(data_dir,"9_Final_Analysis_ID.xlsx"),sheet = 1)
 Final_IDs <- unique(FinalID_df$study_id)
 
 #########################################################################################################
 #3. Load outcome/event type data
 #########################################################################################################
-updated_All_event_df <- read.xlsx(paste0(outdir,"4_updated_All_event_df.xlsx"),sheet = 1)
+updated_All_event_df <- read.xlsx(paste0(data_dir,"4_updated_All_event_df.xlsx"),sheet = 1)
 
 
 ################################################################################ 
@@ -59,9 +61,8 @@ print(paste0("Total number of monthes:", nrow(All_perMonthData_df)))
 ################################################################################ 
 #4. Add charatersitics for the machine learning model (pareall)
 ################################################################################
-#site_cols<- paste0("C",seq(501,509,1))
+site_cols<- paste0("C",seq(501,509,1))
 foreach (i = 1: length(Final_IDs)) %dopar% {
-  i <- 1
   curr_id <- Final_IDs[i]
   
   #curr per month data 
@@ -69,96 +70,84 @@ foreach (i = 1: length(Final_IDs)) %dopar% {
   #curr patient level char
   curr_pt_level_char_df <- Patient_Char_df[which(Patient_Char_df$study_id == curr_id),]
   
+  #construct per patient month level data
+  curr_month_level_char_df <- as.data.frame(matrix(NA, nrow =nrow(curr_perMonth_data) ,ncol = 32))
+  colnames(curr_month_level_char_df) <- c("study_id","num_claims","Age","months_since_dx",
+                                     "Race", site_cols,
+                                     "regional","Grade","Laterality",
+                                     "er_stat","pr_stat","her2_stat","surg_prim_site",
+                                     "DAJCC_T","DAJCC_M","DAJCC_N",
+                                     "reg_age_at_dx","reg_nodes_exam","reg_nodes_pos",
+                                     "cs_tum_size","cs_tum_ext","cs_tum_nodes",
+                                     "months_to_second_event",
+                                     "y_PRE_OR_POST_2ndEvent")
   
-}
-
-################################################################################
-#4. Add charatersitics for the machine learning model
-################################################################################
-site_cols<- paste0("C",seq(501,509,1))
-month_level_char_df <- as.data.frame(matrix(NA, nrow =nrow(All_perMonthData_df) ,ncol = 32))
-colnames(month_level_char_df) <- c("study_id","num_claims","Age","months_since_dx",
-                                   "Race", site_cols,
-                                   "regional","Grade","Laterality",
-                                   "er_stat","pr_stat","her2_stat","surg_prim_site",
-                                   "DAJCC_T","DAJCC_M","DAJCC_N",
-                                   "reg_age_at_dx","reg_nodes_exam","reg_nodes_pos",
-                                   "cs_tum_size","cs_tum_ext","cs_tum_nodes",
-                                   "months_to_second_event",
-                                   "y_PRE_OR_POST_2ndEvent")
-for (i in 1:nrow(All_perMonthData_df)){
-  if (i %% 1000 ==0 ){
-    print(i)
-  }
-  curr_id <- All_perMonthData_df[i,"study_id"]
-  month_level_char_df[i,"study_id"] <- curr_id
-
-  #curr patient level char
-  curr_pt_level_char_df <- Patient_Char_df[which(Patient_Char_df$study_id == curr_id),]
-
+  curr_month_level_char_df[,"study_id"] <- curr_id
+  
   #add features
   feature_cols <- c("Race","regional","Grade","Laterality",
                     "er_stat","pr_stat","her2_stat","surg_prim_site",
                     "DAJCC_T","DAJCC_M","DAJCC_N",
                     "reg_age_at_dx","reg_nodes_exam","reg_nodes_pos",
                     "cs_tum_size","cs_tum_ext","cs_tum_nodes")
-  month_level_char_df[i,feature_cols] <- curr_pt_level_char_df[,feature_cols]
-
+  curr_month_level_char_df[,feature_cols] <- curr_pt_level_char_df[,feature_cols]
+  
   #curr site
   curr_site <- curr_pt_level_char_df[,"Site"]
-  month_level_char_df[i,curr_site] <- 1 #curr site col = 1
-  month_level_char_df[i, site_cols[which(site_cols != curr_site)]] <- 0 #othre site 0
-
+  curr_month_level_char_df[,curr_site] <- 1 #curr site col = 1
+  curr_month_level_char_df[, site_cols[which(site_cols != curr_site)]] <- 0 #othre site 0
+  
   #curr event df
   curr_event <- updated_All_event_df[which(updated_All_event_df[,"study_id"] == curr_id),]
   #first and 2nd event and death date
   curr_1stevent_date <- mdy(curr_event[,"Date_1st_Event"])
   curr_2ndevent_date <- mdy(curr_event[,"Date_2nd_Event"])
-
+  
   #date of birth
   curr_diag_year <- as.numeric(unlist(strsplit(as.character(curr_1stevent_date),split = "-"))[[1]])
   curr_age_at_diags <- curr_pt_level_char_df[,"reg_age_at_dx"]
   curr_dob <- curr_diag_year - curr_age_at_diags
+  
+  
+  #for each  month
+  for (j in 1:nrow(curr_month_level_char_df)){
+    curr_month <- ymd(curr_perMonth_data[j,"Month_Start"])
+    curr_month_yr <- as.numeric(unlist(strsplit(as.character(curr_month),split = "-"))[[1]])
+  
+    curr_month_level_char_df[j,"Age"] <- curr_month_yr - curr_dob
+    curr_month_level_char_df[j,"months_since_dx"] <- as.numeric(difftime(curr_month,curr_1stevent_date,units = "days"))/30 #converted to month
+    if (is.na(curr_2ndevent_date) == F){
+      curr_month_level_char_df[j,"months_to_second_event"] <- as.numeric(difftime(curr_month,curr_2ndevent_date,units = "days"))/30 #converted to month
+      
+    }else{
+      curr_month_level_char_df[j,"months_to_second_event"] <- NA
+    }
+    
+    #get outcome label
+    if (curr_month_level_char_df[j,"months_to_second_event"] < 0 | is.na(curr_month_level_char_df[j,"months_to_second_event"])==T){
+      curr_month_level_char_df[j,"y_PRE_OR_POST_2ndEvent"] <- 0
+    }else{
+      curr_month_level_char_df[j,"y_PRE_OR_POST_2ndEvent"] <- 1
+    }
+    
+    #number of claims in each month
+    curr_perday_file <- paste0(perday_dir,"ID",curr_id,"_perDay_Data.xlsx") #    #1.get per day file
+    
+    if (file.exists(curr_perday_file) == T){
+      curr_perday_df <- read.xlsx(curr_perday_file,sheet = 1)
+      curr_claims <- which(ymd(curr_perday_df$claims_date) >= curr_month & ymd(curr_perday_df$claims_date) < curr_month + days(30))
+      curr_month_level_char_df[j,"num_claims"] <- length(curr_claims)
+      
+    }else{
+      curr_month_level_char_df[j,"num_claims"] <- 0
+    }
+    
+   }
+  
+  curr_comb_df <- cbind(curr_perMonth_data,curr_month_level_char_df[,-1])
 
-  #curernt month
-  curr_month <- ymd(All_perMonthData_df[i,"Month_Start"])
-  curr_month_yr <- as.numeric(unlist(strsplit(as.character(curr_month),split = "-"))[[1]])
-
-  month_level_char_df[i,"Age"] <- curr_month_yr - curr_dob
-  month_level_char_df[i,"months_since_dx"] <- as.numeric(difftime(curr_month,curr_1stevent_date,units = "days"))/30 #converted to month
-  if (is.na(curr_2ndevent_date) == F){
-    month_level_char_df[i,"months_to_second_event"] <- as.numeric(difftime(curr_month,curr_2ndevent_date,units = "days"))/30 #converted to month
-
-  }else{
-    month_level_char_df[i,"months_to_second_event"] <- NA
-  }
-
-  if (month_level_char_df[i,"months_to_second_event"] < 0 | is.na(month_level_char_df[i,"months_to_second_event"])==T){
-    month_level_char_df[i,"y_PRE_OR_POST_2ndEvent"] <- 0
-  }else{
-    month_level_char_df[i,"y_PRE_OR_POST_2ndEvent"] <- 1
-  }
-
-  #number of claims in each month
-  #1.get per day file
-  curr_perday_file <- paste0(perday_dir,"ID",curr_id,"_perDay_Data.xlsx")
-
-  if (file.exists(curr_perday_file) == T){
-    curr_perday_df <- read.xlsx(curr_perday_file,sheet = 1)
-    curr_claims <- which(ymd(curr_perday_df$claims_date) >= curr_month & ymd(curr_perday_df$claims_date) < curr_month + days(30))
-    month_level_char_df[i,"num_claims"] <- length(curr_claims)
-
-  }else{
-    month_level_char_df[i,"num_claims"] <- 0
-  }
-
+  write.xlsx(curr_comb_df,paste0(outdir,"ID",curr_id,"_","PerMonthData_WithMonthChar_df.xlsx"))
+  
 }
 
-
-################################################################################ 
-#5. Combine code sand char
-################################################################################ 
-comb_df <- cbind(All_perMonthData_df,month_level_char_df[,-1])
-
-write.xlsx(comb_df,paste0(outdir,"10_PerMonthData_WithMonthChar_df.xlsx"))
 
