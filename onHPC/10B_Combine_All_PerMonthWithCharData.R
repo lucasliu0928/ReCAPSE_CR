15dbd298c282a6eace09b3e9c657f4ba48619beb
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-07-09 07:50:00 -0400

    Update 10B_Combine_All_PerMonthWithCharData.R

--- onHPC/10B_Combine_All_PerMonthWithCharData.R
+++ onHPC/10B_Combine_All_PerMonthWithCharData.R
@@ -1,11 +1,11 @@
 library(openxlsx)
 
 #onHPC
-data_dir <- "/recapse/intermediate_data/10_perMonthData_withChar/"
+data_dir <- "/recapse/intermediate_data/10_perMonthData_withChar_V2_nonuniquecodes/"
 outdir <- "/recapse/intermediate_data/"
 
 # #local
-# data_dir <- "/Users/lucasliu/Desktop/intermediate_data/10_perMonthData_withChar/"
+# data_dir <- "/Users/lucasliu/Desktop/intermediate_data/10_perMonthData_withChar_V2_nonuniquecodes/"
 # outdir <- "/Users/lucasliu/Desktop/intermediate_data/"
 
 ################################################################################ 
@@ -18,8 +18,7 @@ Final_IDs <- unique(FinalID_df$study_id)
 #1. Combine all per month with char data into one dataframe
 ################################################################################ 
 All_Data_list <- list(NA)
-for (i in 1:length(Final_IDs)){ 
-  if (i %% 1000 == 0){print(i)}
+for (i in 1:length(Final_IDs)){  #length(Final_IDs)
   curr_id <- Final_IDs[i]
   curr_perMonth_file <- paste0(data_dir,"ID",curr_id,"_PerMonthData_WithMonthChar_df.xlsx")
   
@@ -34,4 +33,4 @@ for (i in 1:length(Final_IDs)){
 
 All_Data_df <- do.call(rbind,All_Data_list)
 
-write.xlsx(All_Data_df,paste0(outdir,"10_All_PerMonthData_WithMonthChar_df.xlsx"))
+write.csv(All_Data_df,paste0(outdir,"10_All_PerMonthData_WithMonthChar_df.csv"),row.names = F)
