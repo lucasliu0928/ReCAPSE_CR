03b78a762e49e518086c093aaa2da906d3e6fd70
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-07-10 09:00:34 -0400

    Update 12_Get_CodeCount_Features.R

--- onHPC/12_Get_CodeCount_Features.R
+++ onHPC/12_Get_CodeCount_Features.R
@@ -33,12 +33,12 @@ grouped_unique_proc_df$CCS_CATEGORY <- paste0("CCS_P_",grouped_unique_proc_df$CC
 grouped_unique_drug_df <- read.csv(paste0(data_dir,"11_grouped_unique_drug_df.csv"), stringsAsFactors = F)
 grouped_unique_drug_df$specific_group <- paste0("DM3_SPE_",grouped_unique_drug_df$specific_group)
 
-n <- 5183
-diag_feature_df <- get_code_feature_df_func(All_data[1:n,],grouped_unique_diag_df,"Unique_Diag_Codes","Diag_Codes","CCS_CATEGORY")
+
+diag_feature_df <- get_code_feature_df_func(All_data,grouped_unique_diag_df,"Unique_Diag_Codes","Diag_Codes","CCS_CATEGORY")
 write.csv(diag_feature_df,paste0(outdir,"12_diag_feature_df.csv"),row.names = F)
-proc_feature_df <- get_code_feature_df_func(All_data[1:n,],grouped_unique_proc_df,"Unique_Proc_Codes","Proc_Codes","CCS_CATEGORY")
+proc_feature_df <- get_code_feature_df_func(All_data,grouped_unique_proc_df,"Unique_Proc_Codes","Proc_Codes","CCS_CATEGORY")
 write.csv(proc_feature_df,paste0(outdir,"12_proc_feature_df.csv"),row.names = F)
-drug_feature_df <- get_code_feature_df_func(All_data[1:n,],grouped_unique_drug_df,"Unique_Drug_Codes","Drug_Codes","specific_group")
+drug_feature_df <- get_code_feature_df_func(All_data,grouped_unique_drug_df,"Unique_Drug_Codes","Drug_Codes","specific_group")
 write.csv(drug_feature_df,paste0(outdir,"12_drug_feature_df.csv"),row.names = F)
 
 ##Get race1,race2....,grade2,summ_stage2,laterality9,er_stat0
