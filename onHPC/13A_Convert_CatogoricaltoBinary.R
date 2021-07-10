7e4dc73540390dd57b934cbfd433ff8da5f514c8
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-07-10 09:00:32 -0400

    Update 13A_Convert_CatogoricaltoBinary.R

--- onHPC/13A_Convert_CatogoricaltoBinary.R
+++ onHPC/13A_Convert_CatogoricaltoBinary.R
@@ -6,10 +6,10 @@ grp_dir <- "/recapse/data/Testing data for UH3 - Dec 16 2020/"
 data_dir <- "/recapse/intermediate_data/"
 outdir <- "/recapse/intermediate_data/"
 
-# #local
-grp_dir <- "/Volumes/LJL_ExtPro/Data/Testing data for UH3 - Dec 16 2020/"
-data_dir <- "/Users/lucasliu/Desktop/intermediate_data/"
-outdir <- "/Users/lucasliu/Desktop/intermediate_data/"
+# # #local
+# grp_dir <- "/Volumes/LJL_ExtPro/Data/Testing data for UH3 - Dec 16 2020/"
+# data_dir <- "/Users/lucasliu/Desktop/intermediate_data/"
+# outdir <- "/Users/lucasliu/Desktop/intermediate_data/"
 
 
 
