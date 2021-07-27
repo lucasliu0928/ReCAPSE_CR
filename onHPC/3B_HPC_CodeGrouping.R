633520e2c65a3e4147b5cab0aba38420281039af
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-07-27 14:31:32 -0400

    Update 3B_HPC_CodeGrouping.R

--- onHPC/3B_HPC_CodeGrouping.R
+++ onHPC/3B_HPC_CodeGrouping.R
@@ -6,14 +6,14 @@ source("Recapse_Ultility.R")
 #Data dir
 ################################################################################
 #on HPC
-data_dir <- "/recapse/intermediate_data/3_perDay_PerPatientData/"
+data_dir <- "/recapse/intermediate_data/3A_perDay_PerPatientData/"
 raw_data_dir <- "/recapse/data/Testing data for UH3 - Dec 16 2020/"
 outdir <- "/recapse/intermediate_data/3B_Codes_And_Groups/"
 
-#local
-data_dir <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/3_perDay_PerPatientData/"
-raw_data_dir <- "/Volumes/LJL_ExtPro/Data/Testing data for UH3 - Dec 16 2020/"
-outdir <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/3B_Codes_And_Groups/"
+# #local
+# data_dir <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/3A_perDay_PerPatientData/"
+# raw_data_dir <- "/Volumes/LJL_ExtPro/Data/Testing data for UH3 - Dec 16 2020/"
+# outdir <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/3B_Codes_And_Groups/"
 
 ################################################################################
 #1. Load data
@@ -77,30 +77,18 @@ unique_drug_df[,"Drug_name"] <- trimws(unique_drug_df[,"Drug_name"], which = c("
 ################################################################################
 #3. Code grouping
 ################################################################################
-#1.CCS diagnose codes
-grouped_unique_diag_df <- group_codes_into_CCS_func(unique_diag_df,CCS_Diag_df)
-length(which(is.na(grouped_unique_diag_df$CCS_CATEGORY)==T)) #1594 no grps
-length(unique(grouped_unique_diag_df$CCS_CATEGORY)) #265 grps
-write.csv(grouped_unique_diag_df,paste0(outdir,"CCS_grouped_unique_diag_df.csv"),row.names = F)
+#1. diagnose codes
+grouped_unique_diag_df <- group_codes_into_CCS_func(unique_diag_df,CCS_Diag_df) #add CCS
+grouped_unique_diag_df <- group_codes_into_Chuback_func(grouped_unique_diag_df,Chuback_Diag_df) #add chuback
+grouped_unique_diag_df <- group_codes_into_Ritzwoller_func(grouped_unique_diag_df,Ritzwoller_Diag_df) #add ritzwoller
+write.csv(grouped_unique_diag_df,paste0(outdir,"Unique_Diag_And_Groups.csv"),row.names = F)
 
-#2. CCS  procedure codes
-grouped_unique_proc_df <- group_codes_into_CCS_func(unique_proc_df,CCS_Proc_df)
-length(which(is.na(grouped_unique_proc_df$CCS_CATEGORY)==T)) #10811 no grps
-length(unique(grouped_unique_proc_df$CCS_CATEGORY)) #216 grps
-write.csv(grouped_unique_proc_df,paste0(outdir,"CCS_grouped_unique_proc_df.csv"),row.names = F)
+#2. procedure codes
+grouped_unique_proc_df <- group_codes_into_CCS_func(unique_proc_df,CCS_Proc_df) #add CCS
+grouped_unique_proc_df <- group_codes_into_Chuback_func(grouped_unique_proc_df,Chuback_Proc_df)  #add chuback
+grouped_unique_proc_df <- group_codes_into_Ritzwoller_func(grouped_unique_proc_df,Ritzwoller_Proc_df)#add ritzwoller
+write.csv(grouped_unique_proc_df,paste0(outdir,"Unique_Proc_And_Groups.csv"),row.names = F)
 
-#3. Chuback diagnose codes
-grouped_unique_diag_df_chuback <- group_codes_into_Chuback_func(unique_diag_df,Chuback_Diag_df)
-write.csv(grouped_unique_proc_df,paste0(outdir,"Chuabck_grouped_unique_proc_df.csv"),row.names = F)
-
-#4. Chuback procedure codes
-grouped_unique_proc_df_chuback <- group_codes_into_Chuback_func(unique_proc_df,Chuback_Proc_df)
-
-#5. Ritzwoller diagnose codes
-grouped_unique_diag_df_ritz <- group_codes_into_Ritzwoller_func(unique_diag_df,Ritzwoller_Diag_df)
-#6. Chuback procedure codes
-grouped_unique_proc_df_ritz <- group_codes_into_Ritzwoller_func(unique_proc_df,Ritzwoller_Proc_df)
-
-#6. DM3 drug codes
+#3. DM3 drug codes
 grouped_unique_drug_df <- group_drugcodes_into_DM3_func(unique_drug_df,DM3_df)
-write.csv(grouped_unique_drug_df,paste0(outdir,"DM3_grouped_unique_drug_df.csv"),row.names = F)
+write.csv(grouped_unique_drug_df,paste0(outdir,"Unique_Drug_And_Groups.csv"),row.names = F)
