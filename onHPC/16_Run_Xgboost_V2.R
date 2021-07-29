106d4a772da6c6296e72135e1e8670144b5f40dd
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-07-28 20:35:44 -0400

    Update 16_Run_Xgboost_V2.R

--- onHPC/16_Run_Xgboost_V2.R
+++ onHPC/16_Run_Xgboost_V2.R
@@ -16,12 +16,12 @@ registerDoParallel(numCores)  # use multicore, set to the number of our cores
 #onHPC
 project_dir            <- "/recapse/intermediate_data/"
 modelReady_dir         <- paste0(project_dir,"12_ModelReadyData/")
-outdir                 <- paste0(project_dir,"12_ModelReadyData/")
+outdir                 <- paste0(project_dir,"16_Performance/")
 
 #local
 project_dir            <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 modelReady_dir         <- paste0(project_dir,"12_ModelReadyData/")
-outdir                 <- paste0(project_dir,"12_ModelReadyData/")
+outdir                 <- paste0(project_dir,"16_Performance/")
 
 
 ######################################################################################################## 
@@ -76,42 +76,30 @@ validation_ID <- remaining_ID[which(!remaining_ID %in% training_ID)]
 train_data <- model_data[which(model_data$study_id %in% training_ID),]
 print("Training: : ")
 table(train_data$y_PRE_OR_POST_2ndEvent) 
-# #downsample
-# pos_idxes<- which(train_data$y_PRE_OR_POST_2ndEvent==1)
-# neg_idxes<- which(train_data$y_PRE_OR_POST_2ndEvent==0)
-# sampled_neg_indxes <- sample(neg_idxes,length(pos_idxes))
-# train_data <- train_data[c(sampled_neg_indxes,pos_idxes),]
+train_label <- as.numeric(train_data[,"y_PRE_OR_POST_2ndEvent"])
+train_data_part<-train_data[,!(names(train_data) %in% c("study_id","Month_Start","y_PRE_OR_POST_2ndEvent"))]
+dtrain <- xgb.DMatrix(data = as.matrix(train_data_part), label = train_label)
+
 
 validation_data <- model_data[which(model_data$study_id %in% validation_ID),]
-table(validation_data$y_PRE_OR_POST_2ndEvent) #89164  4572 
+print("Validation: : ")
+table(validation_data$y_PRE_OR_POST_2ndEvent) 
+validation_label <- as.numeric(validation_data[,"y_PRE_OR_POST_2ndEvent"])
+validation_data_part<-validation_data[,!(names(validation_data) %in% c("study_id","Month_Start","y_PRE_OR_POST_2ndEvent"))]
+dvalidation <- xgb.DMatrix(data = as.matrix(validation_data_part), label = validation_label)
+
 
 test_data <- model_data[which(model_data$study_id %in% test_IDs),]
-table(test_data$y_PRE_OR_POST_2ndEvent) #96080  4381 
-
-
+print("Test: : ")
+table(test_data$y_PRE_OR_POST_2ndEvent) 1 
+test_label <- as.numeric(test_data[,"y_PRE_OR_POST_2ndEvent"])
+test_data_part<-test_data[,!(names(test_data) %in% c("study_id","y_PRE_OR_POST_2ndEvent"))]
+dtest <- xgb.DMatrix(data = as.matrix(test_data_part), label = test_label)
 
 
 # ######################################################################################################## 
 #           Teresa's code
 # ######################################################################################################## 
-train_label <- train_data[,"y_PRE_OR_POST_2ndEvent"]
-train_label <- as.numeric(train_label)
-train_data_part<-train_data[,!(names(train_data) %in% c("study_id","y_PRE_OR_POST_2ndEvent"))]
-dtrain <- xgb.DMatrix(data = as.matrix(train_data_part), label = train_label)
-
-validation_label <- validation_data[,"y_PRE_OR_POST_2ndEvent"]
-validation_label <- as.numeric(validation_label)
-validation_data_part<-validation_data[,!(names(validation_data) %in% c("study_id","y_PRE_OR_POST_2ndEvent"))]
-dvalidation <- xgb.DMatrix(data = as.matrix(validation_data_part), label = validation_label)
-
-
-test_label <- test_data[,"y_PRE_OR_POST_2ndEvent"]
-test_label <- as.numeric(test_label)
-test_data_part<-test_data[,!(names(test_data) %in% c("study_id","y_PRE_OR_POST_2ndEvent"))]
-dtest <- xgb.DMatrix(data = as.matrix(test_data_part), label = test_label)
-
-
-
 optimal_results <- BayesianOptimization(xgb_cv_bayes,
                                         bounds=list(eta=c(0.001, 0.3),
                                                     max_depth=c(3L, 10L),
@@ -119,8 +107,6 @@ optimal_results <- BayesianOptimization(xgb_cv_bayes,
                                                     subsample=c(0.3, 0.9), colsample_by_tree=c(0.2, 0.8)),
                                         init_points=10,
                                         n_iter=10)
-
-
 current_best <- list(etc = as.numeric(optimal_results$Best_Par['eta']),
                      max_depth = as.numeric(optimal_results$Best_Par['max_depth']),
                      min_child_weight = as.numeric(optimal_results$Best_Par['min_child_weight']),
@@ -132,41 +118,38 @@ watchlist <- list(train = dtrain, eval = dvalidation)
 mod_optimal <- xgb.train(objective="binary:logistic",
                          params=current_best, data=dtrain, nrounds=10, early_stopping_rounds=100, maximize=TRUE,
                          watchlist=watchlist, verbose=TRUE, print_every_n=10, eval_metric="error", eval_metric="error@0.2", eval_metric="auc")
-
-
-mod_easy <- xgb.train(objective="binary:logistic",
-                      data=dtrain, nrounds=10)
-
-pred <- predict(mod_optimal, dtest)
-actual <-test_label
-
-
+#Prediction table
+pred   <- predict(mod_optimal, dtest)
+actual <- test_label
+prediction_tb <- as.data.frame(cbind(pred,actual))
+write.csv(prediction_tb,paste0(outdir,"16_prediction_tb.csv"),row.names = F)
+#Performance table
 perf <- compute_binaryclass_perf_func(pred,actual)
 print(perf)
 write.csv(perf,paste0(outdir,"16_perf.csv"),row.names = F)
 
-#check 
-bst <- xgboost(data = dtrain,nrounds = 10, params = list(scale_pos_weight = 9),objective = "binary:logistic")
-pred <- predict(bst, dtest)
-perf <- compute_binaryclass_perf_func(pred,actual)
-print(perf)
-
-importance_matrix = xgb.importance(model = mod_optimal)
-
-importance_matrix <- xgb.importance(model = mod_optimal)
-importance_matrix$Group <- as.factor(1)
-gp = xgb.ggplot.importance(importance_matrix,top_n = 10,n_clusters = 1)
-final_p <- gp +geom_bar(stat="identity",position = position_dodge(width=10))+aes(fill = "brown1") +
-  theme_bw()+
-  theme(legend.position = "none",
-        #panel.grid.major = element_blank(), #remove grid
-        plot.title = element_text(size= 22),
-        axis.text.x = element_text(size = 20),
-        axis.text.y = element_text(size = 20),
-        axis.title = element_text(size=20)) +
-  ggtitle("")
-final_p
-
+# #check 
+# bst <- xgboost(data = dtrain,nrounds = 10, params = list(scale_pos_weight = 9),objective = "binary:logistic")
+# pred <- predict(bst, dtest)
+# perf <- compute_binaryclass_perf_func(pred,actual)
+# print(perf)
+# 
+# importance_matrix = xgb.importance(model = mod_optimal)
+# 
+# importance_matrix <- xgb.importance(model = mod_optimal)
+# importance_matrix$Group <- as.factor(1)
+# gp = xgb.ggplot.importance(importance_matrix,top_n = 10,n_clusters = 1)
+# final_p <- gp +geom_bar(stat="identity",position = position_dodge(width=10))+aes(fill = "brown1") +
+#   theme_bw()+
+#   theme(legend.position = "none",
+#         #panel.grid.major = element_blank(), #remove grid
+#         plot.title = element_text(size= 22),
+#         axis.text.x = element_text(size = 20),
+#         axis.text.y = element_text(size = 20),
+#         axis.title = element_text(size=20)) +
+#   ggtitle("")
+# final_p
+# 
 
 
 # #statitiscs
