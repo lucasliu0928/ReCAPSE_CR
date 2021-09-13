8d20ebba3b25378d736c053d0ac2fbf3e8b32cd8
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-09-13 15:50:22 -0400

    Update 2_HPC_GetPerPatientData_Medicaid_HealthClaims.R

--- onHPC/2_HPC_GetPerPatientData_Medicaid_HealthClaims.R
+++ onHPC/2_HPC_GetPerPatientData_Medicaid_HealthClaims.R
@@ -5,20 +5,26 @@ library(doParallel)
 library(openxlsx)
 
 #######################################################################################
-#1.Process Medicaid health claims
-#######################################################################################
 numCores <- detectCores() # get the number of cores available
 print(numCores)
+registerDoParallel(numCores)  # use multicore, set to the number of our cores
+#######################################################################################
+
+
+#######################################################################################
+#1.Process Medicaid health claims
+#######################################################################################
 data_dir <- "/recapse/data/Testing data for UH3 - Dec 16 2020/"
-outdir1 <- "/recapse/intermediate_data/2_perPatientData/Medicaid_HealthClaims/"
+outdir <- "/recapse/intermediate_data/2_RawClaims_perPatient/Medicaid_HealthClaims/"
+
 #Load data
-medicaid_health_df <- as.data.frame(fread(paste0(data_dir,"kcr_medicaid_healthclaims_fb0015.csv")))
+medicaid_health_df <- read.csv(paste0(data_dir,"kcr_medicaid_healthclaims_fb0015.csv"),stringsAsFactors = F)
 health_ID <- unique(medicaid_health_df[,"study_id"])
 #Get per patient data
-registerDoParallel(numCores)  # use multicore, set to the number of our cores
+
 system.time(
 foreach (i = 1: length(health_ID)) %dopar% {
      curr_df <- medicaid_health_df[which(medicaid_health_df[,"study_id"] == health_ID[i]),]
-     write.xlsx(curr_df,paste0(outdir1,"ID",health_ID[i],"_","all_medicaid_healthclaims.xlsx"))
+     write.xlsx(curr_df,paste0(outdir,"ID",health_ID[i],"_","all_medicaid_healthclaims.xlsx"))
 })
 
