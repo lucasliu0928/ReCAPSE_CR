ff8da8dbbff3cf44724fe2dee8d67d012b767f22
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-09-13 15:50:19 -0400

    Update 2_HPC_GetPerPatientData_Medicare.R

--- onHPC/2_HPC_GetPerPatientData_Medicare.R
+++ onHPC/2_HPC_GetPerPatientData_Medicare.R
@@ -1,18 +1,26 @@
 library(openxlsx)
-library(data.table)
-
 data_dir <- "/recapse/data/Testing data for UH3 - Dec 16 2020/"
-outdir <- "/recapse/intermediate_data/2_perPatientData/Medicare/"
+outdir <- "/recapse/intermediate_data/2_RawClaims_perPatient/Medicare/"
+
+# #Local 
+# data_dir <- "/Volumes/LJL_ExtPro/Data/ReCAPSE_Data/Testing data for UH3 - Dec 16 2020/"
+# outdir <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/2_RawClaims_perPatient/Medicare/"
+
 
 #######################################################################################
 #Process Medicare, get per patient data as seperated files
 #######################################################################################
-data_df <- as.data.frame(fread(paste0(data_dir,"kcr_medicare_claims_fb0015.csv")))
+data_df <- read.csv(paste0(data_dir,"kcr_medicare_claims_fb0015.csv"),stringsAsFactors = F)
 unique_IDs <- unique(data_df[,"study_id"])
+print(length(unique_IDs))
 
-system.time(
-for (i in 1:length(unique_IDs)){
-curr_df <- data_df[which(data_df[,"study_id"] == unique_IDs[i]),]
-write.xlsx(curr_df,paste0(outdir,"ID",unique_IDs[i],"_","all_medicare_claims.xlsx"))
+ID_processed <- as.numeric(gsub("_all_medicare_claims.xlsx|ID","",list.files(outdir)))
+if (length(ID_processed) != 0 ){
+  unique_IDs <- unique_IDs[-which(unique_IDs %in% ID_processed)]
 }
-)
+print(length(unique_IDs))
+
+for (i in 1:length(unique_IDs)){ #Can't use foreach, error produced 
+  curr_df <- data_df[which(data_df[,"study_id"] == unique_IDs[i]),]
+  write.xlsx(curr_df,paste0(outdir,"ID",unique_IDs[i],"_","all_medicare_claims.xlsx"))
+}
