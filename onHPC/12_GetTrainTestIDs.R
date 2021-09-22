c3cdf4dae125979d17c782300d8bdea3f58e3513
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-09-22 11:33:22 -0400

    Update 12_GetTrainTestIDs.R

--- onHPC/12_GetTrainTestIDs.R
+++ onHPC/12_GetTrainTestIDs.R
@@ -1,45 +1,4 @@
 source("Recapse_Ultility.R")
-Data_Sampling_Func <- function(upsample_flag,train_data,label_col_name,seed_num,random_perc = 0.8){
-  # upsample_flag <- 0
-  # train_data <- train_data
-  # label_col_name <- "y_PRE_OR_POST_2ndEvent"
-  # seed_num <- 1
-  
-  #Get label col index
-  label_col_index <- which(colnames(train_data) == label_col_name)
-  
-  #Sampling
-  if(upsample_flag==1){ #upsampling
-    set.seed(seed_num)
-    up_train <- upSample(x = train_data[, -label_col_index],
-                         y = as.factor(train_data[,label_col_name]), yname = label_col_name)  
-    sampled_train_data <- up_train
-    
-  }else if(upsample_flag==0){ #downsample
-    set.seed(seed_num)
-    down_train <- downSample(x = train_data[, -label_col_index],
-                             y = as.factor(train_data[,label_col_name]), yname = label_col_name)      
-    sampled_train_data <- down_train
-    
-  }else if(upsample_flag==2){ #random sample 90% of orignal data
-    set.seed(seed_num)
-    sampled_indxes <- sample(nrow(train_data), nrow(train_data)*random_perc, replace = TRUE, prob = NULL)
-    sampled_train_data <- train_data[sampled_indxes,]
-  }else if (upsample_flag == 3){ #random sample then down sample
-    set.seed(seed_num)
-    sampled_indxes <- sample(nrow(train_data), nrow(train_data)*random_perc, replace = TRUE, prob = NULL)
-    randomsampled_train_data <- train_data[sampled_indxes,]
-    down_train <- downSample(x = randomsampled_train_data[, -label_col_index],
-                             y = as.factor(randomsampled_train_data[,label_col_name]), yname = label_col_name)      
-    sampled_train_data <- down_train
-  }else{
-    original_train <- train_data
-    sampled_train_data <- original_train
-  }
-  
-  return(sampled_train_data)
-}
-
 ################################################################################
 #Data dir
 ################################################################################
@@ -73,60 +32,19 @@ original_noSBCE_toSBCEratio <- round(length(nosbce_pt_Ids)/length(sbce_pt_Ids)) 
 
 ######################################################################################################## 
 #3. make sure no overlapping in original Ids in train and test
+#Without sampling
 #Test: 20% of the original IDs
 #Train: 80% of the orginal IDs
 #Validation sets will be auto generated when doing CV
 ########################################################################################################
 total_n <- length(Final_ID)
-
-test_ID  <- sample(Final_ID,0.2*total_n)
-table(pts_level_char_df[which(pts_level_char_df$study_id %in% test_ID),"SBCE"])
-
-#1.Testing : 
 set.seed(123)
-n <- 100
-test_ID_SBCE   <- sample(sbce_pt_Ids,n)
-test_ID_noSBCE <- sample(nosbce_pt_Ids,n*original_noSBCE_toSBCEratio)
-test_IDs <- c(test_ID_SBCE,test_ID_noSBCE)
-print(paste0("# Test Original IDs: ", length(test_IDs)))
-#remove test ID from 
-remaining_ID <- Final_ID[which(!Final_ID %in% test_IDs)]
+test_ID   <- sample(Final_ID,0.2*total_n)
+train_ID  <- sample(Final_ID,0.8*total_n)
 
-#1. Training 80% of the remaining_ID
-training_ID <- sample(remaining_ID,length(remaining_ID)*0.8)
-print(paste0("# Train Original IDs: ", length(training_ID)))
+test_ID_withLabel_df <- pts_level_char_df[which(pts_level_char_df$study_id %in% test_ID),c("study_id" , "SBCE")]
+train_ID_withLabel_df <- pts_level_char_df[which(pts_level_char_df$study_id %in% train_ID),c("study_id" , "SBCE")]
 
-#2. validation 20% of the remaining_ID
-validation_ID <- remaining_ID[which(!remaining_ID %in% training_ID)]
-print(paste0("# Validation Original IDs: ", length(validation_ID)))
+write.xlsx(test_ID_withLabel_df, paste0(outdir,"test_ID_withLabel.xlsx"))
+write.xlsx(train_ID_withLabel_df,paste0(outdir,"train_ID_withLabel.xlsx"))
 
-
-####Get data
-train_data <- model_data[which(model_data$study_id %in% training_ID),]
-print("Training:")
-table(train_data$y_PRE_OR_POST_2ndEvent) 
-#Down sampling
-upsample_flag <- 0
-seed_num <- 123
-train_data <- Data_Sampling_Func(upsample_flag,train_data,"y_PRE_OR_POST_2ndEvent",seed_num)
-print("Down Sampled:")
-table(train_data$y_PRE_OR_POST_2ndEvent) 
-
-#Create xgb input
-train_label <- as.numeric(train_data[,"y_PRE_OR_POST_2ndEvent"])-1
-train_data_part<-train_data[,!(names(train_data) %in% c("study_id","Month_Start","y_PRE_OR_POST_2ndEvent"))]
-dtrain <- xgb.DMatrix(data = as.matrix(train_data_part), label = train_label)
-
-validation_data <- model_data[which(model_data$study_id %in% validation_ID),]
-print("Validation:")
-table(validation_data$y_PRE_OR_POST_2ndEvent) 
-validation_label <- as.numeric(validation_data[,"y_PRE_OR_POST_2ndEvent"])
-validation_data_part<-validation_data[,!(names(validation_data) %in% c("study_id","Month_Start","y_PRE_OR_POST_2ndEvent"))]
-dvalidation <- xgb.DMatrix(data = as.matrix(validation_data_part), label = validation_label)
-
-test_data <- model_data[which(model_data$study_id %in% test_IDs),]
-print("Test:")
-table(test_data$y_PRE_OR_POST_2ndEvent) 
-test_label <- as.numeric(test_data[,"y_PRE_OR_POST_2ndEvent"])
-test_data_part<-test_data[,!(names(test_data) %in% c("study_id","Month_Start","y_PRE_OR_POST_2ndEvent"))]
-dtest <- xgb.DMatrix(data = as.matrix(test_data_part), label = test_label)
