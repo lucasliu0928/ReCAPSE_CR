807073e9ef547ef1ebcf62b5fa319200d8284192
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-09-22 11:33:24 -0400

    Update 16_Run_Xgboost.R

--- onHPC/16_Run_Xgboost.R
+++ onHPC/16_Run_Xgboost.R
@@ -55,97 +55,75 @@ registerDoParallel(numCores)  # use multicore, set to the number of our cores
 proj_dir  <- "/recapse/intermediate_data/"
 
 #local
-proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
+#proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 
 #data dir
-data_dir1        <- paste0(proj_dir, "9_FinalIDs_And_UpdatedPtsChar/")
+data_dir1        <- paste0(proj_dir, "12_TrainTestIDs/")
 data_dir2        <- paste0(proj_dir, "11D_ModelReady_CombFatures/WithPossibleMonthsHasNoCodes/")
 outdir           <- paste0(proj_dir, "16_Performance/")
+
+#User input
+sampling_flag    <- "Down"
 
 ######################################################################################################## 
 #1. Load and combine all patient model ready data
 ######################################################################################################## 
 pt_files <-list.files(data_dir2,full.names = T)
-model_data <- do.call(rbind, lapply(pt_files,read.xlsx))
+#model_data <- do.call(rbind, lapply(pt_files,read.xlsx))
+model_data <- do.call(rbind,mclapply(pt_files, mc.cores= numCores, function(z){read.xlsx(z, sheet = 1)}))
+
 print("Original pre vs post samples: ")
 table(model_data$y_PRE_OR_POST_2ndEvent)
+
+#Add a column for original study ID 
+original_IDs <- strsplit(model_data$sample_id,split = "@")
+model_data$study_id <- sapply(original_IDs, "[[", 1)
 
 ################################################################################ 
 #2. Load patient level char to get SBCE or not to make sure original ID not in both train and validation
 ################################################################################ 
-Final_ID_df <- read.xlsx(paste0(data_dir1,"9_Final_ID1_WithPossibleMonthsHasNoCodes.xlsx"),sheet = 1)
-Final_ID    <- unique(Final_ID_df$study_id)
+train_ID_df <- read.xlsx(paste0(data_dir1,"train_ID_withLabel.xlsx"),sheet = 1)
+test_ID_df  <- read.xlsx(paste0(data_dir1,"test_ID_withLabel.xlsx"),sheet = 1)
+print("Train non-SBCE vs SBCE : ")
+table(train_ID_df$SBCE) #13542  1049 
+print("Test non-SBCE vs SBCE : ")
+table(test_ID_df$SBCE)  #3362  285 
 
-pts_level_char_df <- read.xlsx(paste0(data_dir1,"9_PtsCharForFinalID_WithPossibleMonthsHasNoCodes.xlsx"),sheet = 1)
-pts_level_char_df <- pts_level_char_df[which(pts_level_char_df$study_id %in% Final_ID),] #only keep char for final ID
-print("Original non-SBCE vs SBCE : ")
-table(pts_level_char_df$SBCE) #16917  1322
+train_ID <- paste0("ID", train_ID_df$study_id)
+test_ID  <- paste0("ID", test_ID_df$study_id)
 
-################################################################################ 
-#3. Get SBCE and non-SBCE IDs
-################################################################################ 
-sbce_pt_Ids <-   unique(pts_level_char_df$study_id[which(pts_level_char_df$SBCE == 1)])
-nosbce_pt_Ids <- unique(pts_level_char_df$study_id[which(pts_level_char_df$SBCE == 0)])
-original_noSBCE_toSBCEratio <- round(length(nosbce_pt_Ids)/length(sbce_pt_Ids)) #13: 1
-
-##########START HERE##########
 ######################################################################################################## 
-#### Data reprocessing 0521
-### make sure no overlapping in original Ids in train,validation and test
+#3.Get model data for train and test
 ######################################################################################################## 
-nonrecurrent_pts_data <- model_data[which(model_data$study_id %in% nosbce_pt_Ids),]
-n_no <- nrow(nonrecurrent_pts_data) #619991
-recurrent_pts_data <- model_data[which(model_data$study_id %in% sbce_pt_Ids),]
-n_yes<- nrow(recurrent_pts_data) # 72670
-
-#1.Testing : Randomly choose 100 SBCE original pts data, and 100*8 noSBCE original pt Data
-set.seed(123)
-n <- 100
-test_ID_SBCE <- sample(sbce_pt_Ids,n)
-test_ID_noSBCE <- sample(nosbce_pt_Ids,n*original_noSBCE_toSBCEratio)
-test_IDs <- c(test_ID_SBCE,test_ID_noSBCE)
-print(paste0("# Test Original IDs: ", length(test_IDs)))
-#remove test ID from 
-remaining_ID <- Final_ID[which(!Final_ID %in% test_IDs)]
-
-#1. Training 80% of the remaining_ID
-training_ID <- sample(remaining_ID,length(remaining_ID)*0.8)
-print(paste0("# Train Original IDs: ", length(training_ID)))
-
-#2. validation 20% of the remaining_ID
-validation_ID <- remaining_ID[which(!remaining_ID %in% training_ID)]
-print(paste0("# Validation Original IDs: ", length(validation_ID)))
-
-
-####Get data
-train_data <- model_data[which(model_data$study_id %in% training_ID),]
-print("Training:")
-table(train_data$y_PRE_OR_POST_2ndEvent) 
-#Down sampling
-upsample_flag <- 0
-seed_num <- 123
-train_data <- Data_Sampling_Func(upsample_flag,train_data,"y_PRE_OR_POST_2ndEvent",seed_num)
-print("Down Sampled:")
-table(train_data$y_PRE_OR_POST_2ndEvent) 
-
-#Create xgb input
-train_label <- as.numeric(train_data[,"y_PRE_OR_POST_2ndEvent"])-1
-train_data_part<-train_data[,!(names(train_data) %in% c("study_id","Month_Start","y_PRE_OR_POST_2ndEvent"))]
-dtrain <- xgb.DMatrix(data = as.matrix(train_data_part), label = train_label)
-
-validation_data <- model_data[which(model_data$study_id %in% validation_ID),]
-print("Validation:")
-table(validation_data$y_PRE_OR_POST_2ndEvent) 
-validation_label <- as.numeric(validation_data[,"y_PRE_OR_POST_2ndEvent"])
-validation_data_part<-validation_data[,!(names(validation_data) %in% c("study_id","Month_Start","y_PRE_OR_POST_2ndEvent"))]
-dvalidation <- xgb.DMatrix(data = as.matrix(validation_data_part), label = validation_label)
-
-test_data <- model_data[which(model_data$study_id %in% test_IDs),]
-print("Test:")
+#I. Get data
+#1. Train
+if (sampling_flag ==  "None"){
+  #1. Train data without down sampling
+  train_data <- model_data[which(model_data[,"study_id"] %in% train_ID),]
+  print("Training Pre vs Post:")
+  table(train_data$y_PRE_OR_POST_2ndEvent) 
+}else if (sampling_flag ==  "Down"){
+  #2. Train data with down sampling
+  seed_num <- 123
+  train_data <- model_data[which(model_data[,"study_id"] %in% train_ID),]
+  train_data <- Data_Sampling_Func(0,train_data,"y_PRE_OR_POST_2ndEvent",seed_num)
+  train_data$y_PRE_OR_POST_2ndEvent <- as.numeric(train_data$y_PRE_OR_POST_2ndEvent) -1
+  print("Training DownSampled Pre vs Post:")
+  table(train_data$y_PRE_OR_POST_2ndEvent) 
+}
+#2. Test
+test_data <- model_data[which(model_data[,"study_id"] %in% test_ID),]
+print("Test: Pre vs Post:")
 table(test_data$y_PRE_OR_POST_2ndEvent) 
-test_label <- as.numeric(test_data[,"y_PRE_OR_POST_2ndEvent"])
-test_data_part<-test_data[,!(names(test_data) %in% c("study_id","Month_Start","y_PRE_OR_POST_2ndEvent"))]
-dtest <- xgb.DMatrix(data = as.matrix(test_data_part), label = test_label)
+
+#II. Create xgb input
+train_label      <- as.numeric(train_data[,"y_PRE_OR_POST_2ndEvent"])
+train_data_part  <- train_data[,!(names(train_data) %in% c("study_id","sample_id","y_PRE_OR_POST_2ndEvent"))]
+dtrain           <- xgb.DMatrix(data = as.matrix(train_data_part), label = train_label)
+
+test_label       <- as.numeric(test_data[,"y_PRE_OR_POST_2ndEvent"])
+test_data_part   <- test_data[,!(names(test_data) %in% c("study_id","sample_id","y_PRE_OR_POST_2ndEvent"))]
+dtest            <- xgb.DMatrix(data = as.matrix(test_data_part), label = test_label)
 
 
 # ######################################################################################################## 
@@ -158,39 +136,36 @@ optimal_results <- BayesianOptimization(xgb_cv_bayes,
                                                     subsample=c(0.3, 0.9), colsample_by_tree=c(0.2, 0.8)),
                                         init_points=10,
                                         n_iter=10)
-pos_weight <- 10 #for weight more on pos samples
 current_best <- list(etc = as.numeric(optimal_results$Best_Par['eta']),
                      max_depth = as.numeric(optimal_results$Best_Par['max_depth']),
                      min_child_weight = as.numeric(optimal_results$Best_Par['min_child_weight']),
                      subsample = as.numeric(optimal_results$Best_Par['subsample']),
                      colsample_by_tree = as.numeric(optimal_results$Best_Par['colsample_by_tree']),
-                     scale_pos_weight = pos_weight)
-#We do not have to use this watch list and we do not have do create valdiation set, cuz xgb_cv_bayes set cross-validation set internally
-watchlist <- list(train = dtrain, eval = dvalidation)
-
+                     scale_pos_weight = 10) #for weight more on pos samples
 mod_optimal <- xgb.train(objective="binary:logistic",
                          params=current_best, data=dtrain, nrounds=10, early_stopping_rounds=100, maximize=TRUE,
-                         watchlist=watchlist, verbose=TRUE, print_every_n=10, eval_metric="error", eval_metric="error@0.2", eval_metric="auc")
+                         watchlist= list(train = dtrain, eval = dtest), verbose=TRUE, print_every_n=10, eval_metric="error", eval_metric="error@0.2", eval_metric="auc")
 #Prediction table
 pred   <- predict(mod_optimal, dtest)
 actual <- test_label
-prediction_tb <- cbind.data.frame(study_id = test_data$study_id,Month_Start = test_data$Month_Start,pred,actual)
-write.csv(prediction_tb,paste0(outdir,"16_prediction_tb_withDS_POSweight",pos_weight,".csv"),row.names = F)
+prediction_tb <- cbind.data.frame(sample_id = test_data[,"sample_id"],pred,actual)
+write.csv(prediction_tb,paste0(outdir,"16_Prediction_Table.csv"),row.names = F)
+
 #Performance table 
 perf <- compute_binaryclass_perf_func(pred,actual)
 print(perf)
-write.csv(perf,paste0(outdir,"16_perf_withDS_POSweight",pos_weight,".csv"),row.names = F)
+write.csv(perf,paste0(outdir,"16_Performance_Table.csv"),row.names = F)
 
 #Importantant matrix
 importance_matrix <- xgb.importance(model = mod_optimal)
-write.csv(importance_matrix,paste0(outdir,"16_importance_matrix_withDS_POSweight",pos_weight,".csv"),row.names = F)
+write.csv(importance_matrix,paste0(outdir,"16_importance_matrix",".csv"),row.names = F)
 
 
-
-# #check 
+# 
+# # #check 
 # bst <- xgboost(data = dtrain,nrounds = 10, params = list(scale_pos_weight = 9),objective = "binary:logistic")
 # pred <- predict(bst, dtest)
 # perf <- compute_binaryclass_perf_func(pred,actual)
 # print(perf)
-# 
+
 
