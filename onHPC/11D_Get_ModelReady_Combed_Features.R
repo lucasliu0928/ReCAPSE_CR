2a4a6c48d779a98e5c1dd2ba575dad4e003ae73c
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-10-05 15:27:52 -0400

    Update 11D_Get_ModelReady_Combed_Features.R

--- onHPC/11D_Get_ModelReady_Combed_Features.R
+++ onHPC/11D_Get_ModelReady_Combed_Features.R
@@ -22,12 +22,14 @@ data_dir2  <- paste0(proj_dir, "11B_ModelReady_CharFatures/WithPossibleMonthsHas
 data_dir3  <- paste0(proj_dir, "11C_ModelReady_TransformFatures/WithPossibleMonthsHasNoCodes/")
 data_dir4  <- paste0(proj_dir, "9_FinalIDs_And_UpdatedPtsChar/")
 
-outdir   <- paste0(proj_dir, "11D_ModelReady_CombFatures/WithPossibleMonthsHasNoCodes/")
+#outdir   <- paste0(proj_dir, "11D_ModelReady_CombFatures_WithSurgPrimSite_V1/WithPossibleMonthsHasNoCodes/")
+outdir   <- paste0(proj_dir, "11D_ModelReady_CombFatures_WithSurgPrimSite_V2/WithPossibleMonthsHasNoCodes/")
 
 ################################################################################
 #1.Load all char feature df
 ################################################################################
-All_Char_df <- read.csv(paste0(data_dir2,"All_Binary_Chars.csv"),stringsAsFactors = F)
+#All_Char_df <- read.csv(paste0(data_dir2,"All_Binary_Chars_WithSurgPrimSite_V1.csv"),stringsAsFactors = F)
+All_Char_df <- read.csv(paste0(data_dir2,"All_Binary_Chars_WithSurgPrimSite_V2.csv"),stringsAsFactors = F)
 
 
 ################################################################################
@@ -46,43 +48,46 @@ if (length(ID_processed) != 0 ){
 print(length(analysis_IDs))
 
 
-########################################################################################################################
-#For each pt, generate a dataframe with all selected group feature as columns
-########################################################################################################################
-foreach (i = 1: length(analysis_IDs)) %dopar% {
-  curr_id <- analysis_IDs[i]
-  
-  #groups feature df
-  curr_grpfile <- paste0("ID",curr_id,"_Selected_Grp_Features.xlsx")
-  curr_grpf_df <- read.xlsx(paste0(data_dir1,curr_grpfile),sheet = 1)
-  
-  #get transformation feature df 
-  curr_transffile <- paste0("ID",curr_id,"_Transf_Features.xlsx")
-  curr_transf_df <- read.xlsx(paste0(data_dir3,curr_transffile),sheet = 1)
-  
-  #get char feature df
-  curr_idxes      <- which(All_Char_df[,"study_id"] == curr_id)
-  curr_charf_df   <- All_Char_df[curr_idxes,]
-  
-  #match all the time rows
-  all_time_inOrder <- sort(curr_grpf_df[,"Month_Start"],decreasing = F)
-  curr_grpf_df     <- curr_grpf_df[match(all_time_inOrder, curr_grpf_df[,"Month_Start"]),]
-  curr_transf_df   <- curr_transf_df[match(all_time_inOrder, curr_transf_df[,"Month_Start"]),]
-  curr_charf_df    <- curr_charf_df[match(all_time_inOrder, curr_charf_df[,"Month_Start"]),]
-  
-  #comb df
-  curr_comb_df <- cbind(curr_charf_df,curr_grpf_df,curr_transf_df)
-  
-  #change rowname to sample ID (ptID + rowID)
-  curr_comb_df$sample_id <- paste0("ID",curr_id,"@",all_time_inOrder)
-  
-  #remove redudant columns,keep the 1st Id and month start
-  indexes_toremove <- which(colnames(curr_comb_df) %in% c("study_id","Month_Start"))
-  curr_comb_df <- curr_comb_df[, -indexes_toremove]
-  
-  #reorder column, put sample ID first
-  sample_ID_idxes <- ncol(curr_comb_df)
-  curr_comb_df <- curr_comb_df[, c(sample_ID_idxes,1:(sample_ID_idxes - 1))]
-  write.xlsx(curr_comb_df,paste0(outdir,"ID",curr_id,"_Comb_Features.xlsx"))
+if (length(analysis_IDs) > 0){
+    ########################################################################################################################
+    #For each pt, generate a dataframe with all selected group feature as columns
+    ########################################################################################################################
+    foreach (i = 1: length(analysis_IDs)) %dopar% {
+      curr_id <- analysis_IDs[i]
+      
+      #groups feature df
+      curr_grpfile <- paste0("ID",curr_id,"_Selected_Grp_Features.xlsx")
+      curr_grpf_df <- read.xlsx(paste0(data_dir1,curr_grpfile),sheet = 1)
+      
+      #get transformation feature df 
+      curr_transffile <- paste0("ID",curr_id,"_Transf_Features.xlsx")
+      curr_transf_df <- read.xlsx(paste0(data_dir3,curr_transffile),sheet = 1)
+      
+      #get char feature df
+      curr_idxes      <- which(All_Char_df[,"study_id"] == curr_id)
+      curr_charf_df   <- All_Char_df[curr_idxes,]
+      
+      #match all the time rows
+      all_time_inOrder <- sort(curr_grpf_df[,"Month_Start"],decreasing = F)
+      curr_grpf_df     <- curr_grpf_df[match(all_time_inOrder, curr_grpf_df[,"Month_Start"]),]
+      curr_transf_df   <- curr_transf_df[match(all_time_inOrder, curr_transf_df[,"Month_Start"]),]
+      curr_charf_df    <- curr_charf_df[match(all_time_inOrder, curr_charf_df[,"Month_Start"]),]
+      
+      #comb df
+      curr_comb_df <- cbind(curr_charf_df,curr_grpf_df,curr_transf_df)
+      
+      #change rowname to sample ID (ptID + rowID)
+      curr_comb_df$sample_id <- paste0("ID",curr_id,"@",all_time_inOrder)
+      
+      #remove redudant columns,keep the 1st Id and month start
+      indexes_toremove <- which(colnames(curr_comb_df) %in% c("study_id","Month_Start"))
+      curr_comb_df <- curr_comb_df[, -indexes_toremove]
+      
+      #reorder column, put sample ID first
+      sample_ID_idxes <- ncol(curr_comb_df)
+      curr_comb_df <- curr_comb_df[, c(sample_ID_idxes,1:(sample_ID_idxes - 1))]
+      write.xlsx(curr_comb_df,paste0(outdir,"ID",curr_id,"_Comb_Features.xlsx"))
+      
+    }
   
 }
