69ab646c73a81319bffa80e2c1334f60983843ad
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-10-04 20:01:49 -0400

    Update 9_Get_FinalIDs_ByExclusions.R

--- onHPC/9_Get_FinalIDs_ByExclusions.R
+++ onHPC/9_Get_FinalIDs_ByExclusions.R
@@ -29,7 +29,7 @@ registerDoParallel(numCores)  # use multicore, set to the number of our cores
 proj_dir  <- "/recapse/intermediate_data/"
 
 #local
-proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
+#proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 
 #data dir
 data_dir1  <- paste0(proj_dir, "8_Characteristics2/Patient_Level/")
