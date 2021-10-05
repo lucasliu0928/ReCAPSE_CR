4e1814dd747302edeedb30cecfe5680ccd4770f1
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-10-05 15:27:56 -0400

    Update 17_ComputeDispStats.R

--- onHPC/17_ComputeDispStats.R
+++ onHPC/17_ComputeDispStats.R
@@ -99,8 +99,6 @@ final_Ids <- Final_PTs_Char_df$study_id
 recode_idxes1 <- which(grepl("1Recur",Final_PTs_Char_df[,"Type_2nd_Event"]) == T)
 Final_PTs_Char_df[recode_idxes1,"Type_2nd_Event"] <- "1Recur"
 
-#Recode Race
-Final_PTs_Char_df[-which(Final_PTs_Char_df[,"Race"] %in% c(1,2)),"Race"] <- "Others"
 
 ################################################################################ 
 #Get SBCE and non-SBCE pts char df
@@ -110,6 +108,10 @@ noSBCE_PTs_Char_df <- Final_PTs_Char_df[which(Final_PTs_Char_df[,"SBCE"] == 0),]
 
 ################################################################################ 
 #3. Report some stats
+#'@Updated 100521: 
+#surg_prim_site_V1 and surg_prim_site_V2:
+##Version1 (Dr.Huang): 0, 19, (20-24), 30, (40-42), (50-59,63),  (60-62, 64-69, 73,74), 70-72, 80, 90, 99
+#Version2 (Quan):  00,19,20 (21-24),30,40,41,42,50,51(53-56),52(57,58,59,63),60,61(64-67),62(68,69,73,74),70,71,72,80,90,99
 #'@NOTE: Need to add DAJCC_M,DAJCC_N,DAJCC_T and num_nobc later
 #'@Note: These table does not show all features for training (e.g, age at each month, monthes since diagnosis)
 ################################################################################ 
@@ -120,11 +122,12 @@ all_variables <- c("SBCE","Medicaid_OR_Medicare","reg_age_at_dx","Diagnosis_Year
                    "Type_2nd_Event",
                    "Race" , "Site" , "Stage","Grade",
                    "Laterality" ,"er_stat","pr_stat",	"her2_stat",	
-                    "reg_nodes_exam", "reg_nodes_pos", "surg_prim_site", 
+                    "reg_nodes_exam", "reg_nodes_pos", "surg_prim_site_V1","surg_prim_site_V2",
                     "cs_tum_size", "cs_tum_ext", 
                      "cs_tum_nodes", "regional")
 n_perc_variables <- c("SBCE", "Medicaid_OR_Medicare", "Race","Site","Stage","Grade","Laterality",
-                      "er_stat","pr_stat","her2_stat","surg_prim_site","regional","most_recent_enrollment_year","Diagnosis_Year","Type_2nd_Event")
+                      "er_stat","pr_stat","her2_stat","surg_prim_site_V1","surg_prim_site_V2","regional",
+                      "most_recent_enrollment_year","Diagnosis_Year","Type_2nd_Event")
 
 table_all <- compute_stats_func(Final_PTs_Char_df,"ALL",all_variables,n_perc_variables)
 table_sbce <- compute_stats_func(SBCE_PTs_Char_df,"SBCE",all_variables,n_perc_variables)
@@ -134,7 +137,7 @@ table_comb <- cbind(table_all,table_sbce,table_nonsbce)
 write.csv(table_comb, paste0(outdir,"discrip_table.csv"))
 
 #Histgram
-plot_hist <- function(in_data, x_name, grp_name, xbreaks,x_label){
+plot_hist_twocohort <- function(in_data, x_name, grp_name, xbreaks,x_label){
   p <- ggplot(in_data, aes_string(x=x_name, color=grp_name)) +
     geom_histogram(fill="white", alpha=0.8, position="identity",binwidth = 0.5) +
     scale_x_continuous(breaks= xbreaks) +
@@ -145,14 +148,66 @@ plot_hist <- function(in_data, x_name, grp_name, xbreaks,x_label){
   return(p)
 }
 
-Final_PTs_Char_df$SBCE <- as.character(Final_PTs_Char_df$SBCE)
-p <- plot_hist(Final_PTs_Char_df,"Diagnosis_Year","SBCE",seq(2004, 2015, 1),"Diagnosis Year")
-png(paste0(outdir,"diagnosis_yr.png"), width = 1000, height = 800, res = 120)
-print(p)
-dev.off()
+plot_hist_onecohort <- function(in_data, x_name, xbreaks,x_label,cohort_name, barcolor){
+  p <- ggplot(in_data, aes_string(x=x_name)) +
+    geom_histogram(fill = barcolor ,alpha = 0.8, position="identity",binwidth = 0.5) +
+    scale_x_continuous(breaks= xbreaks) +
+    theme(legend.position="top") + 
+    labs(x = x_label, y = "Count",title = cohort_name) +
+    theme(text = element_text(size=20))
+  return(p)
+}
 
-p <- plot_hist(Final_PTs_Char_df,"most_recent_enrollment_year","SBCE",seq(2005, 2020, 1),"Most Recent Enrollment Year")
-png(paste0(outdir,"most_recent_enroll_yr.png"), width = 1200, height = 800, res = 120)
-print(p)
-dev.off()
+output_hist_forSBCEand_nonSBCE <- function(in_data,plot_colname,x_lab,cohort_name1,cohort_name0, xbreaks, plotwidth){
+  # in_data <- Final_PTs_Char_df
+  # plot_colname <- "Diagnosis_Year"
+  # x_lab <- "Diagnosis Year"
+  # cohort_name1 <- "Recurrent Patient"
+  # cohort_name0 <- "non-Recurrent Patient"
+  
+  SBCE_PTs_Char_df <- in_data[which(in_data$SBCE==1),]
+  p <- plot_hist_onecohort(SBCE_PTs_Char_df,plot_colname,xbreaks, x_lab,cohort_name1,"brown4")
+  png(paste0(outdir,plot_colname, "_", cohort_name1, ".png"), width = plotwidth, height = 800, res = 120)
+  print(p)
+  dev.off()
+  
+  nonSBCE_PTs_Char_df <- in_data[which(in_data$SBCE==0),]
+  p <- plot_hist_onecohort(nonSBCE_PTs_Char_df,plot_colname,xbreaks,x_lab, cohort_name0,"dodgerblue4")
+  png(paste0(outdir,plot_colname, "_", cohort_name0, ".png"), width = plotwidth, height = 800, res = 120)
+  print(p)
+  dev.off()
+}
 
+
+#Plot Diagnosis_Year:
+output_hist_forSBCEand_nonSBCE(Final_PTs_Char_df,"Diagnosis_Year","Diagnosis Year",
+                               "Recurrent Patient","non-Recurrent Patient",seq(2004, 2015, 1),1000)
+
+#Plot most_recent_enrollment_year: #"Both Medicaid Medicare"
+#Medicaid 
+Medicaid_PTs_Char_df <- Final_PTs_Char_df[which(Final_PTs_Char_df$Medicaid_OR_Medicare== "Medicaid"),]
+output_hist_forSBCEand_nonSBCE(Medicaid_PTs_Char_df,"most_recent_enrollment_year","Most Recent Enrollment Year",
+                               "Recurrent Patient (Medicaid)","non-Recurrent Patient (Medicaid)", seq(2005, 2020, 1),1200)
+
+Medicare_PTs_Char_df <- Final_PTs_Char_df[which(Final_PTs_Char_df$Medicaid_OR_Medicare== "Medicare"),]
+output_hist_forSBCEand_nonSBCE(Medicare_PTs_Char_df,"most_recent_enrollment_year","Most Recent Enrollment Year",
+                               "Recurrent Patient (Medicare)","non-Recurrent Patient (Medicare)", seq(2005, 2020, 1),1200)
+
+
+
+Both_PTs_Char_df <- Final_PTs_Char_df[which(Final_PTs_Char_df$Medicaid_OR_Medicare== "Both"),]
+output_hist_forSBCEand_nonSBCE(Both_PTs_Char_df,"most_recent_enrollment_year","Most Recent Enrollment Year",
+                               "Recurrent Patient (Medicare & Medicaid)","non-Recurrent Patient (Medicare & Medicaid)", seq(2005, 2020, 1),1200)
+
+
+# Final_PTs_Char_df$SBCE <- as.character(Final_PTs_Char_df$SBCE)
+# p <- plot_hist_twocohort(Final_PTs_Char_df,"Diagnosis_Year","SBCE",seq(2004, 2015, 1),"Diagnosis Year")
+# png(paste0(outdir,"diagnosis_yr.png"), width = 1000, height = 800, res = 120)
+# print(p)
+# dev.off()
+# 
+# p <- plot_hist_twocohort(Final_PTs_Char_df,"most_recent_enrollment_year","SBCE",seq(2005, 2020, 1),"Most Recent Enrollment Year")
+# png(paste0(outdir,"most_recent_enroll_yr.png"), width = 1200, height = 800, res = 120)
+# print(p)
+# dev.off()
+# 
