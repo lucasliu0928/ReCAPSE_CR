fa3af1f2e913d200f7b99da3110159019dc519ba
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-12-01 16:23:46 -0500

    Create 15_XGBoost_InputData.R

---
+++ onHPC/15_XGBoost_InputData.R
@@ -0,0 +1,162 @@
+source("Recapse_Ultility.R")
+Data_Sampling_Func <- function(upsample_flag,train_data,label_col_name,seed_num,random_perc = 0.8){
+  # upsample_flag <- 0
+  # train_data <- train_data
+  # label_col_name <- "y_PRE_OR_POST_2ndEvent"
+  # seed_num <- 1
+  
+  #Get label col index
+  label_col_index <- which(colnames(train_data) == label_col_name)
+  
+  #Sampling
+  if(upsample_flag==1){ #upsampling
+    set.seed(seed_num)
+    up_train <- upSample(x = train_data[, -label_col_index],
+                         y = as.factor(train_data[,label_col_name]), yname = label_col_name)  
+    sampled_train_data <- up_train
+    
+  }else if(upsample_flag==0){ #downsample
+    set.seed(seed_num)
+    down_train <- downSample(x = train_data[, -label_col_index],
+                             y = as.factor(train_data[,label_col_name]), yname = label_col_name)      
+    sampled_train_data <- down_train
+    
+  }else if(upsample_flag==2){ #random sample 90% of orignal data
+    set.seed(seed_num)
+    sampled_indxes <- sample(nrow(train_data), nrow(train_data)*random_perc, replace = TRUE, prob = NULL)
+    sampled_train_data <- train_data[sampled_indxes,]
+  }else if (upsample_flag == 3){ #random sample then down sample
+    set.seed(seed_num)
+    sampled_indxes <- sample(nrow(train_data), nrow(train_data)*random_perc, replace = TRUE, prob = NULL)
+    randomsampled_train_data <- train_data[sampled_indxes,]
+    down_train <- downSample(x = randomsampled_train_data[, -label_col_index],
+                             y = as.factor(randomsampled_train_data[,label_col_name]), yname = label_col_name)      
+    sampled_train_data <- down_train
+  }else{
+    original_train <- train_data
+    sampled_train_data <- original_train
+  }
+  
+  return(sampled_train_data)
+}
+
+print_n_prepostsamples_func <- function(in_data, data_name){
+  #in_data <- model_data
+  tb      <- table(in_data[,"y_PRE_OR_POST_2ndEvent"])
+  n_pre   <- as.numeric(tb[which(names(tb)==0)])
+  n_post  <- as.numeric(tb[which(names(tb)==1)])
+  
+  print(paste0(data_name, "Pre:" , n_pre, " Post:",n_post))
+}
+
+get_SampleIDs_withDSLabels <-function(in_data,downsampled_sampleIDs,ds_index){
+  model_data_IDandLabels <- in_data[,c("study_id","sample_id","y_PRE_OR_POST_2ndEvent")]
+  
+  #Add downsample train flag col
+  model_data_IDandLabels$DownSampled_Train <- NA
+  train_idxes2 <- which(model_data_IDandLabels[,"sample_id"] %in% downsampled_sampleIDs)
+  model_data_IDandLabels[train_idxes2,"DownSampled_Train"] <- ds_index
+  
+  return(model_data_IDandLabels)
+}
+
+
+################################################################################
+#Set up parallel computing envir
+################################################################################
+numCores <- detectCores() # get the number of cores available
+print(numCores)
+registerDoParallel(numCores)  # use multicore, set to the number of our cores
+
+################################################################################
+#Data dir
+################################################################################
+#onHPC
+proj_dir  <- "/recapse/intermediate_data/"
+
+#local
+#proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
+
+#data dir
+data_dir1        <- paste0(proj_dir, "12_TrainTestIDs/")
+data_dir2        <- paste0(proj_dir, "11D_ModelReady_CombFatures_WithSurgPrimSite_V1_1111updated/WithPossibleMonthsHasNoCodes/")
+outdir           <- paste0(proj_dir, "16_Performance_WithSurgPrimSite_V1_1111updated/")
+
+#data_dir2        <- paste0(proj_dir, "11D_ModelReady_CombFatures_WithSurgPrimSite_V2_1111updated/WithPossibleMonthsHasNoCodes/")
+#outdir           <- paste0(proj_dir, "16_Performance_WithSurgPrimSite_V2_1111updated/")
+
+#User input
+sampling_flag    <- "Down"
+n_sampling       <- 10
+
+######################################################################################################## 
+#1. Load and combine all patient model ready data
+######################################################################################################## 
+pt_files <-list.files(data_dir2,full.names = T)
+model_data <- do.call(rbind,mclapply(pt_files, mc.cores= numCores, function(z){read.xlsx(z, sheet = 1)}))
+
+#Add a column for original study ID 
+original_IDs <- strsplit(model_data$sample_id,split = "@")
+model_data$study_id <- sapply(original_IDs, "[[", 1)
+
+################################################################################ 
+#2. Load patient level char to get SBCE or not to make sure original ID not in both train and validation
+################################################################################ 
+train_ID_df <- read.xlsx(paste0(data_dir1,"train_ID_withLabel.xlsx"),sheet = 1)
+test_ID_df  <- read.xlsx(paste0(data_dir1,"test_ID_withLabel.xlsx"),sheet = 1)
+
+train_ID <- paste0("ID", train_ID_df$study_id)
+test_ID  <- paste0("ID", test_ID_df$study_id)
+
+######################################################################################################## 
+#3.Get model data for train and test
+######################################################################################################## 
+#I. Get data
+#1. Train
+if (sampling_flag ==  "None"){
+  #1. Train data without down sampling
+  train_data <- model_data[which(model_data[,"study_id"] %in% train_ID),]
+  #Print num of pre and post samples 
+  print_n_prepostsamples_func(train_data,"Train: ")
+  
+}else if (sampling_flag ==  "Down"){
+  
+  #2. Train data with down sampling 10 times
+  for (i in 1:n_sampling){
+    seed_num <- 122 + i 
+    #All train
+    train_data <- model_data[which(model_data[,"study_id"] %in% train_ID),]
+    #Down sampled train
+    train_data <- Data_Sampling_Func(0,train_data,"y_PRE_OR_POST_2ndEvent",seed_num)
+    train_data$y_PRE_OR_POST_2ndEvent <- as.numeric(train_data$y_PRE_OR_POST_2ndEvent) -1
+    
+    #Output model ready binary data
+    save(train_data, file=paste0(outdir, "XGB_Input/", "train_data","_DS" ,i,".rda"))
+    
+    #Output Sample IDs and Label for later stats
+    train_IDsandLabels <- train_data[,c("study_id","sample_id","y_PRE_OR_POST_2ndEvent")]
+    write.csv(train_IDsandLabels,paste0(outdir,"XGB_Input/TrainSampleIDsAndLabels" , i , ".csv"),row.names = F)
+    
+
+    #Print num of pre and post samples 
+    print_n_prepostsamples_func(train_data,"Train: ")
+  }
+  
+  #'@TODELTE
+  # #Output model data ID and labels and down sampled train flag
+  # for (i in 1:n_sampling){
+  #   model_data_IDandLabels <- get_SampleIDs_withDSLabels(model_data,train_sample_IDs[[i]], i)
+  #   write.csv(model_data_IDandLabels,paste0(outdir,"DownSampled_TrainInfo/Model_Data_WithDSFlag.csv"),row.names = F)
+  # }
+}
+
+
+
+
+#2. Test
+test_data <- model_data[which(model_data[,"study_id"] %in% test_ID),]
+#Print num of pre and post samples 
+print_n_prepostsamples_func(test_data,"Test: ")
+
+#Output model ready binary data
+save(test_data, file=paste0(outdir, "XGB_Input/test_data.rda"))
