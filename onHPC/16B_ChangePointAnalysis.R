4b74036483af72561469152fd44edf700623aecf
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-12-03 03:33:08 -0500

    Update 16B_ChangePointAnalysis.R

--- onHPC/16B_ChangePointAnalysis.R
+++ onHPC/16B_ChangePointAnalysis.R
@@ -13,13 +13,13 @@ proj_dir  <- "/recapse/intermediate_data/"
 proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 
 #data dir
-data_dir1        <- paste0(proj_dir, "16_Performance_WithSurgPrimSite_V1_1201updated/All_DS_Performance/")
+data_dir1        <- paste0(proj_dir, "16_Performance_WithSurgPrimSite_V1_1201updated/Use_ImportantFs_Performance/")
 data_dir2        <- paste0(proj_dir, "12C_TrainTestIDs/")
 
-outdir           <- paste0(proj_dir, "16_Performance_WithSurgPrimSite_V1_1201updated/All_DS_Performance/")
+outdir           <- paste0(proj_dir, "16_Performance_WithSurgPrimSite_V1_1201updated/Use_ImportantFs_Performance/")
 
 #User Input
-ds_index <- 0
+ds_index <- 1
 
 ######################################################################################################## 
 #1.Load predictions 
