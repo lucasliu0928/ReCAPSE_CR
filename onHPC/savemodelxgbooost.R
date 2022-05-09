5fee22b458a5bf943ec436f8469db84fc4a00791
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-05-09 16:03:01 -0400

    Create savemodelxgbooost.R

---
+++ onHPC/savemodelxgbooost.R
@@ -0,0 +1,10 @@
+library(xgboost)
+data(agaricus.train, package='xgboost')
+data(agaricus.test, package='xgboost')
+train <- agaricus.train
+test <- agaricus.test
+bst <- xgboost(data = train$data, label = train$label, max_depth = 2,
+               eta = 1, nthread = 2, nrounds = 2,objective = "binary:logistic")
+xgb.save(bst, 'xgb.model')
+bst <- xgb.load('xgb.model')
+pred <- predict(bst, test$data)
