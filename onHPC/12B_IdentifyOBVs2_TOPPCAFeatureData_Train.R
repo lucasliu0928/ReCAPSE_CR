7b713ef6c0afa06f87b88c58a13f39e8bae7be72
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-05-08 22:17:21 -0400

    Update 12B_IdentifyOBVs2_TOPPCAFeatureData_Train.R

--- onHPC/12B_IdentifyOBVs2_TOPPCAFeatureData_Train.R
+++ onHPC/12B_IdentifyOBVs2_TOPPCAFeatureData_Train.R
@@ -22,16 +22,22 @@ proj_dir  <- "/recapse/intermediate_data/"
 #data dir
 data_dir  <- paste0(proj_dir, "11E_AllPTs_ModelReadyData/WithPossibleMonthsHasNoCodes/")
 data_dir2 <- paste0(proj_dir, "11F_TrainTestIDs/")
-data_dir3 <- paste0(proj_dir, "12A_PCA_TSNE_Analysis/WithPossibleMonthsHasNoCodes/")
+data_dir3 <- paste0(proj_dir, "12A_PCA_VarContri_Train/WithPossibleMonthsHasNoCodes/")
 
-newout <- "12B_TopPCAFeature_ModelReady_TrainData/WithPossibleMonthsHasNoCodes/"
+newout <- "12B_TopPCAFeatureData_Train/WithPossibleMonthsHasNoCodes/"
 outdir   <- paste0(proj_dir, newout)
 dir.create(file.path(proj_dir, newout), recursive = TRUE)
 
+
 ######################################################################################################## 
-#1. Load all pts model data
+#1.Load PCA feature contribution data
 ######################################################################################################## 
-#1A. Load data
+feature_contribution_PCA <- read.csv(paste0(data_dir3,"PCA_Variable_Contribution.csv"),stringsAsFactors = F)
+
+######################################################################################################## 
+#1. Get model data for train data only
+######################################################################################################## 
+#1A. Load all pts data
 load(file = paste0(data_dir, "All_PTS_ModelReadyData.rda"))
 label_col   <- "y_PRE_OR_POST_2ndEvent"
 
@@ -48,34 +54,27 @@ model_data[,label_col] <- factor(model_data[,label_col],levels = c("Pre", "Post"
 #1D.Change label name
 colnames(model_data)[which(colnames(model_data) == label_col)] <- "Label" #Change label col name for plot
 
-######################################################################################################## 
-#2. Load train patient IDs
-######################################################################################################## 
+#1E. Load train patient IDs
 train_ID_df <- read.xlsx(paste0(data_dir2,"train_ID_withLabel.xlsx"),sheet = 1)
 train_ID_df$study_id <- paste0("ID",train_ID_df$study_id)
+train_IDs <- unique(train_ID_df[,"study_id"])
 
-######################################################################################################## 
-#3.Updated model data for train data only
-######################################################################################################## 
-model_data <- model_data[model_data[,"study_id"] %in% train_ID_df[,"study_id"],]
+#1F.Updated model data for train data only
+model_data <- model_data[model_data[,"study_id"] %in% train_IDs,]
 table(model_data$Label)
 
-######################################################################################################## 
-#2.Load feature contribution data
-######################################################################################################## 
-#2A Load data
-feature_contribution_PCA <- read.csv(paste0(data_dir3,"PCA_Variable_Contribution.csv"),stringsAsFactors = F)
 
 ######################################################################################################## 
-#3.Output model ready data for top 2 features on dim1, dim 2 and Dim1 
+#3.Output model ready data for top 2 features on dim1 and dim 2
 ######################################################################################################## 
+#'@TODO: Get top 2 features from contribution dataframe
 model_data_4f <- model_data[,c("study_id","sample_id",
                                "cumul_ratio_CCS_PROC_202",
                                "cumul_ratio_CCS_PROC_227",
                                "months_since_dx",
                                "Enrolled_year",
                                "Label")]
-save(model_data_4f, file=paste0(outdir, "4F_ModelReady_TrainData.rda"))
+save(model_data_4f, file=paste0(outdir, "TopPCAFeature_ModelReadyData_Train.rda"))
 
 
 ######################################################################################################## 
@@ -101,6 +100,4 @@ for (i in 1:nrow(model_data)){
 }
 
 weighted_sum_df <- model_data[,c("study_id","sample_id","Dim1Top10Fs_WeightedSumScore",paste0("Normed_",top10Fs_dim1))]
-write.csv(weighted_sum_df,paste0(outdir,"Dim1_Weighted_Sum_feature.csv"))
-save(weighted_sum_df, file=paste0(outdir, "Dim1_Weighted_Sum_feature.rda"))
-
+save(weighted_sum_df, file=paste0(outdir, "PCADim1WSF_ModelReadyData_Train.rda"))
