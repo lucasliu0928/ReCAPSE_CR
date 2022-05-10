e4c6437c42a841c6cca6704ecbd1659e4f7137c0
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-05-10 03:37:05 -0400

    Update 16D_Prediction_TestData.R

--- onHPC/16D_Prediction_TestData.R
+++ onHPC/16D_Prediction_TestData.R
@@ -1,5 +1,8 @@
 source("Recapse_Ultility.R")
-
+#this script make predictions for test data using 2 methods:
+#1. Method1: AI methods: AI models to predict all test
+#2. Method2: hybrid methods: AI model to predict non-obv, obv_neg/pos are predicted as neg/pos 
+#3. For the results of method1 and method2, then do curve fitting to fine-tune the results
 
 ################################################################################
 #Set up parallel computing envir
@@ -15,7 +18,7 @@ registerDoParallel(numCores)  # use multicore, set to the number of our cores
 proj_dir  <- "/recapse/intermediate_data/"
 
 #local
-proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
+#proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 
 #data dir
 data_dir1        <- paste0(proj_dir, "15_XGB_Input/")
@@ -26,10 +29,11 @@ outdir   <- paste0(proj_dir, newout)
 dir.create(file.path(proj_dir, newout), recursive = TRUE)
 
 #For each training set model
-for (ds_index in 1:1){
+for (ds_index in 1:10){
   #Create out dir for each ds index
-  ds_out <- paste0(proj_dir, newout,"train_DS",ds_index,"/")
-  dir.create(file.path(ds_out))
+  ds_out <- paste0("DS",ds_index,"/Prediction_Table/")
+  dir.create(file.path(outdir, ds_out), recursive = TRUE)
+
   ################################################################################
   #1. Load data
   ################################################################################
@@ -62,12 +66,13 @@ for (ds_index in 1:1){
   pred_df_pos <- prediction_2method_func(test_pos_df,features,mod_optimal,"POS")
   pred_df_nonobv <- prediction_2method_func(test_nonobv_df,features,mod_optimal,"nonOBV")
   pred_df_all <- rbind(pred_df_neg,pred_df_pos,pred_df_nonobv)
+  #'@Check
   check <- compare_obvs_samples_2methods_perf(pred_df_all,"nonOBV")
   print(check)
-  #Curve-fitting methods
-  #pred_df_all <- curve_fitting_func(pred_df_all)
+  #Curve-fitting on the two methods' results
+  pred_df_all <- curve_fitting_func(pred_df_all)
   
-  #write.csv(pred_df_all,paste0(ds_out, "pred_tb_all.csv"))
+  write.csv(pred_df_all,paste0(outdir, ds_out, "pred_tb_all.csv"))
   
 }
 
