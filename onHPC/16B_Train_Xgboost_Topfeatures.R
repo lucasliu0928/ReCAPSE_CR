2252e60d7e1bc762fddbee4ca49710b70dc3d052
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-05-10 03:37:09 -0400

    Update 16B_Train_Xgboost_Topfeatures.R

--- onHPC/16B_Train_Xgboost_Topfeatures.R
+++ onHPC/16B_Train_Xgboost_Topfeatures.R
@@ -14,7 +14,7 @@ registerDoParallel(numCores)  # use multicore, set to the number of our cores
 proj_dir  <- "/recapse/intermediate_data/"
 
 #local
-proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
+#proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 
 #data dir
 data_dir1        <- paste0(proj_dir, "15_XGB_Input/")
@@ -26,7 +26,7 @@ outdir   <- paste0(proj_dir, newout)
 dir.create(file.path(proj_dir, newout), recursive = TRUE)
 
 #Run XGBoost 10 times for 10 Downsampled Training data and the none DS training data
-for (i in 1:1){#0:10
+for (i in 1:10){#0:10
   ################################################################################
   #Load train and test
   ################################################################################
@@ -38,35 +38,18 @@ for (i in 1:1){#0:10
   ################################################################################
   important_f_df <- read.csv(paste0(data_dir2,"importance_matrix_DS", i ,".csv"), stringsAsFactors = F)
   important_f_df <- important_f_df[order(important_f_df[,"Gain"],decreasing = T),]
-  top_fs <- important_f_df[1:50,"Feature"]
+  top_fs <- important_f_df[1:10,"Feature"]
 
   ################################################################################
   #Create xgb input
   ################################################################################
   train_label      <- as.numeric(train_data[,"y_PRE_OR_POST_2ndEvent"])
-  train_data_part  <- train_data[,top_fs] #top 50 features
+  train_data_part  <- train_data[,top_fs] #top features
   dtrain           <- xgb.DMatrix(data = as.matrix(train_data_part), label = train_label)
 
   ######################################################################################################## 
-  # Run XGboost          Teresa's code
+  # re-train XGboost with top features
   ######################################################################################################## 
-  # optimal_results <- BayesianOptimization(xgb_cv_bayes,
-  #                                         bounds=list(eta=c(0.001, 0.3),
-  #                                                     max_depth=c(3L, 10L),
-  #                                                     min_child_weight=c(0L, 20L),
-  #                                                     subsample=c(0.3, 0.9)), 
-  #                                                     #colsample_bytree=c(0.2, 0.8)),
-  #                                         init_points=10,
-  #                                         n_iter=10)
-  #Get best paramters from CV
-  current_best <- list(#eta = as.numeric(optimal_results$Best_Par['eta']),
-                       max_depth = as.numeric(optimal_results$Best_Par['max_depth']),
-                       min_child_weight = as.numeric(optimal_results$Best_Par['min_child_weight']),
-                       subsample = as.numeric(optimal_results$Best_Par['subsample']),
-                       #colsample_bytree = as.numeric(optimal_results$Best_Par['colsample_bytree']))
-                       scale_pos_weight = 0.5)
-  
-  #Optimal model
   mod_optimal <- xgb.train(objective="binary:logistic",
                            params=list(scale_pos_weight = 0.5),
                            data=dtrain, 
