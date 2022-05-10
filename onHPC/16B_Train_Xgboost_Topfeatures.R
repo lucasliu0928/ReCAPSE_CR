4ba0689fbd7f0482b838081e4edabc282a42e016
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-05-10 14:08:23 -0400

    Update 16B_Train_Xgboost_Topfeatures.R

--- onHPC/16B_Train_Xgboost_Topfeatures.R
+++ onHPC/16B_Train_Xgboost_Topfeatures.R
@@ -26,7 +26,7 @@ outdir   <- paste0(proj_dir, newout)
 dir.create(file.path(proj_dir, newout), recursive = TRUE)
 
 #Run XGBoost 10 times for 10 Downsampled Training data and the none DS training data
-for (i in 1:10){#0:10
+for (i in 0:10){#0:10
   ################################################################################
   #Load train and test
   ################################################################################
@@ -48,16 +48,33 @@ for (i in 1:10){#0:10
   dtrain           <- xgb.DMatrix(data = as.matrix(train_data_part), label = train_label)
 
   ######################################################################################################## 
-  # re-train XGboost with top features
+  # Run XGboost          Teresa's code
   ######################################################################################################## 
+  optimal_results <- BayesianOptimization(xgb_cv_bayes,
+                                          bounds=list(eta=c(0.001, 0.3),
+                                                      max_depth=c(3L, 10L),
+                                                      min_child_weight=c(0L, 20L),
+                                                      subsample=c(0.3, 0.9)), 
+                                          #colsample_bytree=c(0.2, 0.8)),
+                                          init_points=10,
+                                          n_iter=10)
+  #Get best paramters from CV
+  current_best <- list(eta = as.numeric(optimal_results$Best_Par['eta']),
+                       max_depth = as.numeric(optimal_results$Best_Par['max_depth']),
+                       min_child_weight = as.numeric(optimal_results$Best_Par['min_child_weight']),
+                       subsample = as.numeric(optimal_results$Best_Par['subsample']),
+                       #colsample_bytree = as.numeric(optimal_results$Best_Par['colsample_bytree']))
+                       scale_pos_weight = 0.5)
+  
+  #Optimal model
   mod_optimal <- xgb.train(objective="binary:logistic",
-                           params=list(scale_pos_weight = 0.5),
+                           params=current_best,
                            data=dtrain, 
                            nrounds=10, 
                            early_stopping_rounds=100, 
                            maximize=TRUE,
                            watchlist= list(train = dtrain),
-                           verbose=TRUE, print_every_n=10, eval_metric="error")
+                           verbose=TRUE, print_every_n=10, eval_metric="auc")
 
   #Output model
   xgb.save(mod_optimal, paste0(proj_dir, newout,"train_DS",i,"_topf",".model"))
