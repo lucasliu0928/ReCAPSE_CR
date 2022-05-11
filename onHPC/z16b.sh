fe67f1517b779cce786c459fdd945569933cbc6b
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-05-11 05:14:25 -0400

    Create z16b.sh

---
+++ onHPC/z16b.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# V.Gazula 1/8/2019
+
+#SBATCH -t 800:00:00                             #Time for the job to run
+#SBATCH --job-name=val2nd                        #Name of the job
+#SBATCH -N 1                                    #Number of nodes required
+#SBATCH -n 1
+#SBATCH -c 30                            #Number of cores needed for the job
+#SBATCH --mem=120g                       # 32 GB ram asked
+#SBATCH --nodelist="hpc-4" 				 #Name of the queue hpc-2
+#SBATCH --partition=debug                          
+#SBATCH --mail-type ALL                         #Send email on start/end
+#SBATCH --mail-user jli394@uky.edu               #Where to send email
+
+
+/usr/bin/Rscript --vanilla 16B_Train_Xgboost_Topfeatures.R
+
+#python transfer_input-1.py
