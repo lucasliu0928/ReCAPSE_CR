1203fd74d106c5a529499e63e25ea2ea249d4338
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-06-13 11:13:33 -0400

    Update 11F_Get_TrainTestIDs.R

--- onHPC/11F_Get_TrainTestIDs.R
+++ onHPC/11F_Get_TrainTestIDs.R
@@ -10,7 +10,9 @@ proj_dir  <- "/recapse/intermediate_data/"
 
 #data dir
 data_dir1        <- paste0(proj_dir, "9_FinalIDs_And_UpdatedPtsChar/")
-newout <- "11F_TrainTestIDs/"
+
+SBCE_col <- "SBCE_Excluded_DeathLabel"  #Choose SBCE or SBCE_Excluded_DeathLabel
+newout <- paste0("11F_TrainTestIDs/",SBCE_col,"/")
 outdir   <- paste0(proj_dir, newout)
 dir.create(file.path(proj_dir, newout), recursive = TRUE)
 
@@ -27,13 +29,13 @@ Final_ID    <- unique(Analysis_df$study_id) #18239
 pts_level_char_df <- read.xlsx(paste0(data_dir1,"9_PtsCharForFinalID_WithPossibleMonthsHasNoCodes.xlsx"),sheet = 1,sep.names = " ")
 pts_level_char_df <- pts_level_char_df[which(pts_level_char_df$study_id %in% Final_ID),] #only keep char for final ID
 print("Original non-SBCE vs SBCE : ")
-table(pts_level_char_df$SBCE) #16917  1322
+table(pts_level_char_df[,SBCE_col]) #16917  1322
 
 ################################################################################ 
 #2. Get SBCE and non-SBCE IDs
 ################################################################################ 
-sbce_pt_Ids <-   unique(pts_level_char_df$study_id[which(pts_level_char_df$SBCE == 1)])
-nosbce_pt_Ids <- unique(pts_level_char_df$study_id[which(pts_level_char_df$SBCE == 0)])
+sbce_pt_Ids <-   unique(pts_level_char_df$study_id[which(pts_level_char_df[,SBCE_col] == 1)])
+nosbce_pt_Ids <- unique(pts_level_char_df$study_id[which(pts_level_char_df[,SBCE_col] == 0)])
 original_noSBCE_toSBCEratio <- round(length(nosbce_pt_Ids)/length(sbce_pt_Ids)) #10: 1
 
 ######################################################################################################## 
@@ -48,11 +50,11 @@ set.seed(123)
 test_ID   <- sample(Final_ID,0.2*total_n)
 train_ID  <- Final_ID[-which(Final_ID %in% test_ID)]
 
-test_ID_withLabel_df <- pts_level_char_df[which(pts_level_char_df$study_id %in% test_ID),c("study_id" , "SBCE")]
-train_ID_withLabel_df <- pts_level_char_df[which(pts_level_char_df$study_id %in% train_ID),c("study_id" , "SBCE")]
+test_ID_withLabel_df <- pts_level_char_df[which(pts_level_char_df$study_id %in% test_ID),c("study_id" , SBCE_col)]
+train_ID_withLabel_df <- pts_level_char_df[which(pts_level_char_df$study_id %in% train_ID),c("study_id" ,SBCE_col)]
 
 write.xlsx(test_ID_withLabel_df, paste0(outdir,"test_ID_withLabel.xlsx"))
 write.xlsx(train_ID_withLabel_df,paste0(outdir,"train_ID_withLabel.xlsx"))
 
-table(train_ID_withLabel_df$SBCE) #13555  1037 
-table(test_ID_withLabel_df$SBCE) #3362  285 
+table(train_ID_withLabel_df[,SBCE_col]) #13555  1037 
+table(test_ID_withLabel_df[,SBCE_col]) #3362  285 
