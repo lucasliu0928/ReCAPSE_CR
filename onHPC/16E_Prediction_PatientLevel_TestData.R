e19625e3e5e3a36cf8e171dd77f338b702253a9d
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-06-13 18:50:58 -0400

    Update 16E_Prediction_PatientLevel_TestData.R

--- onHPC/16E_Prediction_PatientLevel_TestData.R
+++ onHPC/16E_Prediction_PatientLevel_TestData.R
@@ -19,8 +19,16 @@ proj_dir  <- "/recapse/intermediate_data/"
 #local
 #proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 
+SBCE_col    <- "SBCE_Excluded_DeathLabel" #choose SBCE or SBCE_Excluded_DeathLabel
+feature_set_name <- "CCSandVAL2nd"
+if (SBCE_col == "SBCE"){
+  label_col   <- "y_PRE_OR_POST_2ndEvent"  
+}else{
+  label_col   <- "y_PRE_OR_POST_2ndEvent_ExcludedDeath"   
+}
+
 #data dir
-data_dir1 <- paste0(proj_dir, "16C_Predictions/Test_0610/")
+data_dir1 <- paste0(proj_dir, "16C_Predictions/",feature_set_name,"/",SBCE_col,"/Test/")
 data_dir2 <- paste0(proj_dir, "8_Characteristics2/Patient_Level/")
 
 outdir    <- data_dir1
@@ -40,7 +48,7 @@ for (ds_index in 0:10){
   #Create out dir for each ds 
   ds_out <- paste0("DS",ds_index,"/Patient_Prediction_Table/")
   dir.create(file.path(outdir, ds_out), recursive = TRUE)
-  
+  print(ds_out)
   for (model in model_list){
     model_pred_file <- paste0("pred_tb_",model,".csv")
     pred_col <- paste0("pred_Method_",model)
@@ -50,24 +58,24 @@ for (ds_index in 0:10){
     ds_pred_df <- read.csv(paste0(ds_in,model_pred_file),stringsAsFactors = F)
     ds_pred_df[,"month_start"] <- ymd(ds_pred_df[,"month_start"])
     
-    #2.Get acutal SBCE and SBCE month
-    sbce_df <- get_pt_actual_sbcelabel_month(ds_pred_df,pts_level_char_df)
+    #2.Get actual SBCE and SBCE month
+    sbce_df <- get_pt_actual_sbcelabel_month(ds_pred_df,pts_level_char_df,SBCE_col)
     
-    #3. Get  predictioned month and label by the first month that the prediction probability is greater or equal to threholds
+    #3. Get  predicted month and label by the first month that the prediction probability is greater or equal to threholds
     thres_list <- seq(1,9,1)
     method_name <- "OneMonth_GT_Threshold"
-    pt_pred_df1 <- get_allpt_level_pred(ds_pred_df,sbce_df,thres_list,method_name)
+    pt_pred_df1 <- get_allpt_level_pred(ds_pred_df,sbce_df,thres_list,method_name,SBCE_col)
     write.csv(pt_pred_df1,paste0(outdir, ds_out,model,"_",method_name,"_patientlevel_pred_tb.csv"))
     
-    #4. Get predictied month and predicted label by persisit 3 months
+    #4. Get predicted month and predicted label by persisit 3 months
     thres_list <- seq(1,9,1)
     method_name <- "Persis3Month_GT_Threshold"
-    pt_pred_df2 <-  get_allpt_level_pred(ds_pred_df,sbce_df,thres_list,method_name)
+    pt_pred_df2 <-  get_allpt_level_pred(ds_pred_df,sbce_df,thres_list,method_name,SBCE_col)
     write.csv(pt_pred_df2,paste0(outdir, ds_out,model,"_",method_name,"_patientlevel_pred_tb.csv"))
     
-    #5. Get predictied month and predicted label by change point anlaysis
+    #5. Get predicted month and predicted label by change point analysis
     method_name <- "BinSeg"
-    pt_pred_df3 <-  get_allpt_level_pred(ds_pred_df,sbce_df,thres_list,method_name)
+    pt_pred_df3 <-  get_allpt_level_pred(ds_pred_df,sbce_df,thres_list,method_name,SBCE_col)
     write.csv(pt_pred_df3,paste0(outdir, ds_out,model,"_",method_name,"_patientlevel_pred_tb.csv"))
     
 
