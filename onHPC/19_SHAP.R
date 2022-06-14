d40fb6a693dc3d39aa83c82b6d584aa92c8e589c
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-06-13 23:14:43 -0400

    Update 19_SHAP.R

--- onHPC/19_SHAP.R
+++ onHPC/19_SHAP.R
@@ -5,6 +5,51 @@ library("xgboost")
 library("data.table")
 library("here")
 
+#Change feature name to the actual group name
+change_feautre_name <- function(indata,ccs_disp_df){
+  # indata <- shap_input_df
+  # ccs_disp_df <- ccs_all_df
+  # 
+  #Change all VAL_2ND to GPI2ND
+  colnames(indata) <- gsub("VAL_2ND","GPI2ND",colnames(indata))
+  
+  #Change all cumul_ratio/time_since/time_until
+  colnames(indata) <- gsub("cumul_ratio_","(Cumulative Ratio)",colnames(indata))
+  colnames(indata) <- gsub("time_since_", "(Time Since)",colnames(indata))
+  colnames(indata) <- gsub("time_until_", "(Time Until)",colnames(indata))
+  
+  #Find the discription of CCS codes
+  updated_names <- NA
+  for (i in 1:length(colnames(indata))){
+    cur_f <-  colnames(indata)[i]
+
+    if (grepl("(",cur_f,fixed = T) == T & grepl("GPI|DM3", cur_f) == F){
+        cur_transf_type <- paste0(strsplit(cur_f, ')')[[1]][1],")")
+        cur_f <- strsplit(cur_f, ')')[[1]][2]
+        curr_code_type <- paste0(strsplit(cur_f,"_")[[1]][1:2],collapse = "_")
+        
+        if (grepl("CCS",curr_code_type)== T){
+          curr_disp <- ccs_disp_df[which(ccs_disp_df[,"Code_Grp"] == cur_f),"Grp_Discrip"]
+        }else{
+          curr_disp <- NA
+        }
+        cur_new_name <- paste0(curr_disp,"[",curr_code_type,"]", cur_transf_type)
+    }else if (grepl("GPI|DM3", cur_f) == T){
+      cur_transf_type <- paste0(strsplit(cur_f, ')')[[1]][1],")")
+      curr_code_type <- strsplit(strsplit(cur_f,"_")[[1]][1],")")[[1]][2]
+      curr_disp <- strsplit(cur_f,"_")[[1]][2]
+      cur_new_name <- paste0(curr_disp,"[",curr_code_type,"]", cur_transf_type)
+      
+    }else{
+      cur_new_name <- cur_f
+    }
+    updated_names[i] <- cur_new_name
+  }
+
+  colnames(indata) <- updated_names
+  
+  return(indata)
+}
 
 ################################################################################
 #Set up parallel computing envir
@@ -12,6 +57,21 @@ library("here")
 numCores <- detectCores() # get the number of cores available
 print(numCores)
 registerDoParallel(numCores)  # use multicore, set to the number of our cores
+
+
+################################################################################ 
+#User input
+#'@NOTE: ds_index = 1 for SBCE_Excluded_DeathLabel
+#'       ds_index = 3 for SBCE
+################################################################################ 
+SBCE_col    <- "SBCE" #choose SBCE or SBCE_Excluded_DeathLabel
+feature_set_name <- "CCSandVAL2nd"
+if (SBCE_col == "SBCE"){
+  label_col   <- "y_PRE_OR_POST_2ndEvent"  
+}else{
+  label_col   <- "y_PRE_OR_POST_2ndEvent_ExcludedDeath"   
+}
+ds_index <- 3
 
 ################################################################################
 #Data dir
@@ -23,19 +83,24 @@ proj_dir  <- "/recapse/intermediate_data/"
 #proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 
 #data dir
-data_dir1 <- paste0(proj_dir, "15_XGB_Input/")
-data_dir2 <- paste0(proj_dir, "16B_Trained_ImportantFeatureModel/")
+data_dir1 <- paste0(proj_dir,"15_XGB_Input/",feature_set_name,"/",SBCE_col,"/")
+data_dir2 <- paste0(proj_dir,"16B_Trained_ImportantFeatureModel/",feature_set_name,"/",SBCE_col,"/")
+data_dir3 <- paste0(proj_dir,"/10G_Counts_UniqueGrp_PtsLevel/WithPossibleMonthsHasNoCodes/")
 
-outdir <- paste0(proj_dir, "17_Performance/")
-
-################################################################################ 
-#User input
-################################################################################ 
-ds_index <- 3
+outdir <- paste0(proj_dir, "17_Performance/",feature_set_name,"/",SBCE_col,"/")
 
 #Create directory
 ds_out <- paste0("DS",ds_index,"/SHAP/")
 dir.create(file.path(outdir, ds_out), recursive = TRUE)
+
+################################################################################
+#Load feature CCS group discrption.names, drug disription is already in the feature name
+################################################################################
+ccs_diag <- read.xlsx(paste0(data_dir3 , "Count_CCS_Diag_Unique_Grps.xlsx"))
+ccs_proc <- read.xlsx(paste0(data_dir3 , "Count_CCS_proc_Unique_Grps.xlsx"))
+ccs_all_df <- rbind(ccs_diag[,c(1,6)],ccs_proc[,c(1,6)])
+ccs_all_df[,"Grp_Discrip"] <- gsub("'","",ccs_all_df[,"Grp_Discrip"])
+ccs_all_df[,"Grp_Discrip"] <- trimws(ccs_all_df[,"Grp_Discrip"])
 
 ################################################################################
 #Load train and test
@@ -60,12 +125,12 @@ mod_optimal <- xgb.load(paste0(data_dir2,"train_DS",ds_index,"_topf",".model"))
 train_data <- train_nonobv_ds_df
 test_data <- rbind(test_pos_df,test_neg_df,test_nonobv_df)
   
-train_label      <- as.numeric(train_data[,"y_PRE_OR_POST_2ndEvent"])
-train_data_part  <- train_data[,top_fs] #top 50 features
+train_label      <- as.numeric(train_data[,label_col])
+train_data_part  <- train_data[,top_fs] #top features
 dtrain           <- xgb.DMatrix(data = as.matrix(train_data_part), label = train_label)
 
-test_label       <- as.numeric(test_data[,"y_PRE_OR_POST_2ndEvent"])
-test_data_part   <- test_data[,top_fs] #top 50 features
+test_label       <- as.numeric(test_data[,label_col])
+test_data_part   <- test_data[,top_fs] #top features
 dtest            <- xgb.DMatrix(data = as.matrix(test_data_part), label = test_label)
 
 test_data_neg <- test_data[which(test_data[,"sample_id"] %in% test_neg_df$sample_id),]
@@ -77,6 +142,7 @@ test_data_nonobv <- test_data[which(test_data[,"sample_id"] %in% test_nonobv_df$
 #SHAP for Train
 ################################################################################
 shap_input_df <- as.matrix(train_data_part)
+shap_input_df <- change_feautre_name(shap_input_df,ccs_all_df)
 
 # To return the SHAP values and ranked features by mean|SHAP|
 shap_values <- shap.values(xgb_model = mod_optimal, X_train = shap_input_df)
@@ -94,6 +160,7 @@ dev.off()
 #SHAP for Test
 ################################################################################
 shap_input_df <- as.matrix(test_data_part) ##make sure the feature is the same order as the ones in the model
+shap_input_df <- change_feautre_name(shap_input_df,ccs_all_df)
 
 # To return the SHAP values and ranked features by mean|SHAP|
 shap_values <- shap.values(xgb_model = mod_optimal, X_train = shap_input_df)
@@ -117,7 +184,7 @@ for (i in 1:3){
   curr_data        <- test_data_list[[i]]
   curr_test_data_type   <- test_data_type[i]
   
-  test_label       <- as.numeric(curr_data[,"y_PRE_OR_POST_2ndEvent"])
+  test_label       <- as.numeric(curr_data[,label_col])
   test_data_part   <- curr_data[,top_fs] #top 50 features
   dtest            <- xgb.DMatrix(data = as.matrix(test_data_part), label = test_label)
   
