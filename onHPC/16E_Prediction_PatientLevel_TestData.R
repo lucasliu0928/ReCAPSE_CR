26cd8c77746dcb9b2c8bba8f39cbb8bc43355c77
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-08-05 23:27:41 -0400

    Update 16E_Prediction_PatientLevel_TestData.R

--- onHPC/16E_Prediction_PatientLevel_TestData.R
+++ onHPC/16E_Prediction_PatientLevel_TestData.R
@@ -19,16 +19,19 @@ proj_dir  <- "/recapse/intermediate_data/"
 #local
 #proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 
-SBCE_col    <- "SBCE_Excluded_DeathLabel" #choose SBCE or SBCE_Excluded_DeathLabel
+SBCE_ID_Folder    <- "SBCE_Excluded_DeathPts" #Choose SBCE or SBCE_Excluded_DeathLabel or SBCE_Excluded_DeathPts
 feature_set_name <- "CCSandVAL2nd"
-if (SBCE_col == "SBCE"){
-  label_col   <- "y_PRE_OR_POST_2ndEvent"  
+if ((SBCE_ID_Folder == "SBCE") | (SBCE_ID_Folder == "SBCE_Excluded_DeathPts")){
+  label_col   <- "y_PRE_OR_POST_2ndEvent" 
+  SBCE_col <- "SBCE"
 }else{
-  label_col   <- "y_PRE_OR_POST_2ndEvent_ExcludedDeath"   
+  label_col   <- "y_PRE_OR_POST_2ndEvent_ExcludedDeath" 
+  SBCE_col <- "SBCE_Excluded_DeathLabel"
 }
 
+
 #data dir
-data_dir1 <- paste0(proj_dir, "16C_Predictions/",feature_set_name,"/",SBCE_col,"/Test/")
+data_dir1 <- paste0(proj_dir, "16C_Predictions/",feature_set_name,"/",SBCE_ID_Folder,"/Test/")
 data_dir2 <- paste0(proj_dir, "8_Characteristics2/Patient_Level/")
 
 outdir    <- data_dir1
