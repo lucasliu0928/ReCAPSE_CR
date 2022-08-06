3c65b9c0c933fe4d1dea609e5e52fffd91a10240
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-08-05 23:27:38 -0400

    Update 17D_PlotPredictions.R

--- onHPC/17D_PlotPredictions.R
+++ onHPC/17D_PlotPredictions.R
@@ -33,23 +33,28 @@ plot_individual_prediction <- function(pt_prediction_df, acutal_pre_post_label_c
 }
 
 
+
 ################################################################################ 
 #User input
+#'@NOTE: ds_index = 1 for SBCE_Excluded_DeathLabel
+#'       ds_index = 3 for SBCE
+#'       ds_index = 5 for SBCE_Excluded_DeathPts
 ################################################################################ 
-SBCE_col    <- "SBCE_Excluded_DeathLabel" #choose SBCE or SBCE_Excluded_DeathLabel
+SBCE_ID_Folder    <- "SBCE_Excluded_DeathPts" #Choose SBCE or SBCE_Excluded_DeathLabel or SBCE_Excluded_DeathPts
 feature_set_name <- "CCSandVAL2nd"
-
-
 model <- "AI"                            #c("Hybrid","AI","HybridCurveFit","AICurveFit")
 method <- "Persis3Month_GT_Threshold"    #c("BinSeg","OneMonth_GT_Threshold","Persis3Month_GT_Threshold")
 ths <- seq(1,9,1)
-ds_index <- 1
+ds_index <- 5  
 
-if (SBCE_col == "SBCE"){
-  label_col   <- "y_PRE_OR_POST_2ndEvent"  
+if ((SBCE_ID_Folder == "SBCE") | (SBCE_ID_Folder == "SBCE_Excluded_DeathPts")){
+  label_col   <- "y_PRE_OR_POST_2ndEvent" 
+  SBCE_col <- "SBCE"
 }else{
-  label_col   <- "y_PRE_OR_POST_2ndEvent_ExcludedDeath"   
+  label_col   <- "y_PRE_OR_POST_2ndEvent_ExcludedDeath" 
+  SBCE_col <- "SBCE_Excluded_DeathLabel"
 }
+
 
 ################################################################################
 #Data dir
@@ -61,8 +66,8 @@ proj_dir  <- "/recapse/intermediate_data/"
 #proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 
 #data dir
-data_dir1 <- paste0(proj_dir, "16C_Predictions/",feature_set_name,"/",SBCE_col,"/Test/")
-outdir    <- paste0(proj_dir,"17_Performance/",feature_set_name,"/",SBCE_col, "/")
+data_dir1 <- paste0(proj_dir, "16C_Predictions/",feature_set_name,"/",SBCE_ID_Folder,"/Test/")
+outdir    <- paste0(proj_dir,"17_Performance/",feature_set_name,"/",SBCE_ID_Folder, "/")
 
 
 ################################################################################ 
