0425cd66831569e6d36b7a750c196e5b83c20e2b
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-09-12 16:43:58 -0400

    Update 11F_Get_TrainTestIDs.R

--- onHPC/11F_Get_TrainTestIDs.R
+++ onHPC/11F_Get_TrainTestIDs.R
@@ -6,30 +6,43 @@ source("Recapse_Ultility.R")
 proj_dir  <- "/recapse/intermediate_data/"
 
 #local
-#proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
+proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/ReCAPSE_Project/ReCAPSE_Intermediate_Data/0610_21/"
 
-#data dir
-data_dir1        <- paste0(proj_dir, "9_FinalIDs_And_UpdatedPtsChar/")
 
 #'@NOTE: 
 #'SBCE: consider there types of events
 #'SBCE_Excluded_DeathLabel: treat death label as 0
 #'SBCE_Excluded_DeathPts: exclude pts with sole death event 
+
+feature_set_name <- "CCSandDM3SPE"    #choose from CCSandDM3SPE , CCSandVAL2nd
+sample_name <- "Samples_HasAtLeastOneCodeGrpFeature"           #choose from "All_Samples" , "Samples_HasAtLeastOneCodeGrpFeature"
 SBCE_col <- "SBCE_Excluded_DeathPts"  #Choose SBCE or SBCE_Excluded_DeathLabel or SBCE_Excluded_DeathPts
-newout <- paste0("11F_TrainTestIDs/",SBCE_col,"/")
+
+#data dir
+data_dir1        <- paste0(proj_dir, "9_FinalIDs_And_UpdatedPtsChar/")
+data_dir2        <- paste0(proj_dir,"11E_AllPTs_ModelReadyData/",feature_set_name,"/", sample_name, "/")
+
+newout <- paste0("11F_TrainTestIDs/",feature_set_name, "/",sample_name, "/",SBCE_col,"/")
 outdir   <- paste0(proj_dir, newout)
 dir.create(file.path(proj_dir, newout), recursive = TRUE)
 
-# ################################################################################ 
-# #1. Load analysis ID
-# ################################################################################ 
-# Analysis_df <- read.xlsx(paste0(data_dir1,"9_Final_ID1_WithPossibleMonthsHasNoCodes.xlsx"),sheet = 1,sep.names = " ")
-# Final_ID    <- unique(Analysis_df$study_id) #18239
-
+################################################################################
+#1. Load Model ready data to get study IDs
+#All Samples and Samples_HasAtLeastOneCodeGrpFeature has different number of study IDs
+################################################################################
+load(file = paste0(data_dir2, "All_PTS_ModelReadyData.rda")) 
+if (grepl("Samples_HasAtLeastOneCodeGrpFeature",data_dir2) == T){
+  analysis_id <- unique(model_data_excluded$study_id)
+}else{
+  analysis_id <- unique(model_data$study_id)
+}
+analysis_id <- as.numeric(gsub("ID","",analysis_id))
+  
 ################################################################################ 
-#4. Pts char
+#4. Pts char, keep data for analysis_id
 ################################################################################ 
 pts_level_char_df <- read.xlsx(paste0(data_dir1,"9_PtsCharForFinalID_WithPossibleMonthsHasNoCodes.xlsx"),sheet = 1,sep.names = " ")
+pts_level_char_df <- pts_level_char_df[which(pts_level_char_df[,"study_id"] %in% analysis_id),]
 
 #'@NOTE: Some patient who died and also get other 2nd event(1st recur), ttey are still kept,
 #'Only exclude patient who has sole event = death
