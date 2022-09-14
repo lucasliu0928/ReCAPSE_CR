6a3d2d79d1a39ddd92414a0888214560a67e1e21
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-09-13 22:52:29 -0400

    Update run2.sh

--- onHPC/run2.sh
+++ onHPC/run2.sh
@@ -3,7 +3,7 @@
 # V.Gazula 1/8/2019
 
 #SBATCH -t 800:00:00                             #Time for the job to run
-#SBATCH --job-name=val2nd                        #Name of the job
+#SBATCH --job-name=DM3SBCEexc                        #Name of the job
 #SBATCH -N 1                                    #Number of nodes required
 #SBATCH -n 1
 #SBATCH -c 30                            #Number of cores needed for the job
@@ -14,6 +14,6 @@
 #SBATCH --mail-user jli394@uky.edu               #Where to send email
 
 
-/usr/bin/Rscript --vanilla 11D_Get_ModelReady_Combed_Features.R
+/usr/bin/Rscript --vanilla 16E_Prediction_PatientLevel_TestData_Runing2.R
 
 #python transfer_input-1.py
