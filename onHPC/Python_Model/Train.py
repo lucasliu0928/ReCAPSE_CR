89523a8689d6a8f60c6c773daf10e72ef7fb809a
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2023-04-13 17:36:04 -0400

    Update Train.py

--- onHPC/Python_Model/Train.py
+++ onHPC/Python_Model/Train.py
@@ -19,6 +19,7 @@ from Ultility_Funcs.TrainTest_Funcs import train_model_cvsearch,get_default_impo
 import joblib
 import os
 import argparse
+import pandas as pd
 
 #python3 Train.py -loc Local -fs CCSandVAL2nd -sc SBCE -mn XGB -top_n 10 -ds 3 -ps Grid
 #python3 Train.py -loc Local -fs CCSandVAL2nd -sc SBCE_Excluded_DeathPts -mn XGB -top_n 10 -ds 5 -ps Grid
@@ -27,41 +28,41 @@ import argparse
 
 
 if __name__ == '__main__':
-    # #############################################################################
-    # #Argments parser
-    # #############################################################################
-    # my_parser = argparse.ArgumentParser(allow_abbrev=False)  #Construct the argument parser
+    #############################################################################
+    #Argments parser
+    #############################################################################
+    my_parser = argparse.ArgumentParser(allow_abbrev=False)  #Construct the argument parser
     
     
-    # my_parser.add_argument("-loc" , type = str , required=True, help="Data Location (e.g., 'Server', 'Local')")
-    # my_parser.add_argument("-fs" , type = str ,  required=True, help="Feature set (e.g., 'CCSandVAL2nd', 'CCSandDM3SPE')")
-    # my_parser.add_argument("-sc" , type = str ,  required=True, help="SBCE column (e.g., 'SBCE', 'SBCE_Excluded_DeathPts','SBCE_Excluded_DeathLabel')")
-    # my_parser.add_argument("-mn" , type = str ,  required=True, help="Model name (e.g., 'RF','XGB')")
-    # my_parser.add_argument("-top_n" , type = int ,  required=True, help="Num of top ranked feature (e.g.10,20,...)")
-    # my_parser.add_argument("-ds" , type = int ,  required=True, help="Index of Down Sampled non-obv sample (e.g.0,1,2,...10, DS0 is the original non-obv without any ds)")
-    # my_parser.add_argument("-ps" , type = str ,  required=True, help="Hyperparameter Search Algorithm (e.g Grid, Bayes)")
+    my_parser.add_argument("-loc" , type = str , required=True, help="Data Location (e.g., 'Server', 'Local')")
+    my_parser.add_argument("-fs" , type = str ,  required=True, help="Feature set (e.g., 'CCSandVAL2nd', 'CCSandDM3SPE')")
+    my_parser.add_argument("-sc" , type = str ,  required=True, help="SBCE column (e.g., 'SBCE', 'SBCE_Excluded_DeathPts','SBCE_Excluded_DeathLabel')")
+    my_parser.add_argument("-mn" , type = str ,  required=True, help="Model name (e.g., 'RF','XGB')")
+    my_parser.add_argument("-top_n" , type = int ,  required=True, help="Num of top ranked feature (e.g.10,20,...)")
+    my_parser.add_argument("-ds" , type = int ,  required=True, help="Index of Down Sampled non-obv sample (e.g.0,1,2,...10, DS0 is the original non-obv without any ds)")
+    my_parser.add_argument("-ps" , type = str ,  required=True, help="Hyperparameter Search Algorithm (e.g Grid, Bayes)")
 
     
-    # args = vars(my_parser.parse_args())       # Parse the argument
-    # #####################################################################################
-    # #Command line input or mannual input
-    # #####################################################################################
-    # location = args['loc']  
-    # feature_sets = args['fs']
-    # SBCE_col = args['sc']
-    # model_name = args['mn']
-    # top_f_num = args['top_n']
-    # ds_indxes = args['ds']
-    # search_alg = args['ps']
+    args = vars(my_parser.parse_args())       # Parse the argument
+    #####################################################################################
+    #Command line input or mannual input
+    #####################################################################################
+    location = args['loc']  
+    feature_sets = args['fs']
+    SBCE_col = args['sc']
+    model_name = args['mn']
+    top_f_num = args['top_n']
+    ds_indxes = args['ds']
+    search_alg = args['ps']
     
-    #Local
-    location = "Local"
-    feature_sets = "CCSandVAL2nd"
-    SBCE_col = "SBCE_Excluded_DeathPts"
-    model_name = "XGB"
-    top_f_num = 10
-    ds_indxes = 5
-    search_alg = "Bayes"
+    # #Local
+    # location = "Local"
+    # feature_sets = "CCSandVAL2nd"
+    # SBCE_col = "SBCE_Excluded_DeathPts"
+    # model_name = "XGB"
+    # top_f_num = 10
+    # ds_indxes = 5
+    # search_alg = "Bayes"
     
     if SBCE_col == "SBCE" or SBCE_col == "SBCE_Excluded_DeathPts":
       label_col   = "y_PRE_OR_POST_2ndEvent"  
@@ -107,7 +108,8 @@ if __name__ == '__main__':
     #Train nonobv
     train_X = train_X2
     train_Y = train_Y2
-    optimal_model, best_para_df = train_model_cvsearch(train_X,train_Y, model_name,search_alg)
+    optimal_model, best_para = train_model_cvsearch(train_X,train_Y, model_name,search_alg)
+    best_para_df = pd.DataFrame.from_dict(best_para, orient = 'index', columns = ['value'])
 
     
     #Output optimal model
@@ -126,7 +128,7 @@ if __name__ == '__main__':
     top_f = list(importance_df["Feature"].iloc[0:top_f_num]) #20
     train_X = train_X[top_f]
     train_Y = train_Y 
-    topf_model = train_topf_model(train_X, train_Y, best_para_df, model_name)
+    topf_model = train_topf_model(train_X, train_Y, best_para, model_name)
 
     #Output optimal model
     joblib.dump(topf_model,  outdir2 + model_name +'_TopFeature_model.pkl')
